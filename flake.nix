0712d23d0c0ed27412c7cec646de9f0640a78675
GetPsyched <priyanshu@getpsyched.dev>
2023-12-19 23:40:39 +0530

    feat: nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    flakey-devShells.url = "https://flakehub.com/f/GetPsyched/not-so-flakey-devshells/0.x.x.tar.gz";
+    flakey-devShells.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, flakey-devShells, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      flakey-devShell-pkgs = flakey-devShells.outputs.packages.${system};
+    in
+    {
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = [
+          (flakey-devShell-pkgs.default.override { environments = [ "nix" "nextjs" ]; })
+          (flakey-devShell-pkgs.vscodium.override { environments = [ "nix" "nextjs" ]; })
+        ];
+      };
+    };
+}
