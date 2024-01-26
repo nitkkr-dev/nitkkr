be2cba36309013ba684c26c53e04980eecbb4e0e
Bhavay Goel <goelbhavay@gmail.com>
2024-01-26 18:11:52 +0530

    Add nextauth_url to env validation

--- env/server.ts
+++ env/server.ts
@@ -6,6 +6,7 @@ export const env = createEnv({
         GOOGLE_CLIENT_ID: z.string(),
         GOOGLE_CLIENT_SECRET: z.string(),
         NEXTAUTH_SECRET: z.string(),
+        NEXTAUTH_URL: z.string().url(),
       },
       experimental__runtimeEnv: process.env,
 });
