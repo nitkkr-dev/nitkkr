4423a946e3f6b926a89cad1ac1e50e919cd142f8
GetPsyched <priyanshu@getpsyched.dev>
2024-01-29 03:26:40 +0530

    fix: don't warn about unused vars for boilerplate code

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(request: Request) {
   return Response.json({
     message: "Welcome to NIT-KKR's official public API",
