bbec60407244f317c86de37fcbbdee0c27c6e7ea
Priyanshu Tripathi <priyanshu@getpsyched.dev>
2024-01-29 03:28:13 +0530

    fix: eslint rules (#46)
    * fix: don't warn to use arrow functions
    
    * fix: don't warn about unused vars for boilerplate code

--- app/departments/[name]/page.tsx
+++ app/departments/[name]/page.tsx
@@ -1,5 +1,6 @@
 import WorkInProgress from '@/components/work-in-progress';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function Department({ params }: { params: { name: string } }) {
   return <WorkInProgress />;
 }
