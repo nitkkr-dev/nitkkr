b791e2b6eaf311a914867ac0e08824f711ea3c68
SilentJungle399 <vardhan.harsh4041@gmail.com>
2024-01-29 17:38:41 +0530

    Added nitkkr.ac.in to remote hosts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nitkkr.ac.in',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
