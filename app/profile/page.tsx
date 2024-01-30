9b06526348f64c4115d841f8e111effb3b36ec75
Bhavay Goel <97433542+bhavaygoel@users.noreply.github.com>
2024-01-30 14:31:59 +0530

    feature: authorisation (basic) (#26)
    * Add basic auth
    
    * Add T3 env
    
    separated server and client variables
    
    * added unauthorized page
    
    * Add Profile dummy page
    which need authorisation
    
    * Add nextauth_url to env validation
    
    * Format code
    
    ---------
    
    Co-authored-by: geekysilento <adityakarna.ak@gmail.com>

---
+++ app/profile/page.tsx
@@ -0,0 +1,11 @@
+import { getServerSession } from 'next-auth';
+
+import { authOptions } from '@/api/auth/auth';
+import Unauthorized from '@/components/unauthorized';
+import WorkInProgress from '@/components/work-in-progress';
+
+export default async function Profile() {
+  const session = await getServerSession(authOptions);
+
+  return session ? <WorkInProgress /> : <Unauthorized />;
+}
