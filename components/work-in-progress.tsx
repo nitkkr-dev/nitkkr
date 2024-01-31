04764d4ca79e7427cf661f3cb97503fee69fb768
Mohit Mahipal <mohit95876@gmail.com>
2024-01-31 15:48:39 +0530

    className fixed

--- components/work-in-progress.tsx
+++ components/work-in-progress.tsx
@@ -3,7 +3,7 @@ import { getLocale } from './dictionaries';
 export default async function WorkInProgress({ lang }: { lang: string }) {
   const dict = await getLocale(lang);
   return (
-    <div>
+    <div className="m-auto max-w-fit text-center">
       <h1>{dict.WIP}</h1>
       <p>{dict.WIP_}</p>
     </div>
