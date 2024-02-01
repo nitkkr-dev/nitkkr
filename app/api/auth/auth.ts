fb8281b6394bc51b7828bffbbafe6de118d5bbf1
Bhavay Goel <97433542+bhavaygoel@users.noreply.github.com>
2024-02-01 21:17:00 +0530

    Fix: Custom error when ENV var not found

--- app/api/auth/auth.ts
+++ app/api/auth/auth.ts
@@ -1,10 +1,16 @@
 import GoogleProvider from 'next-auth/providers/google';
 
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error(
+    'GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET must be defined in your environment.'
+  );
+}
+
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       profile(profile) {
         return {
           id: profile.email,
