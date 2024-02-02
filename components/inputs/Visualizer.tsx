f700fab86be63a1d956357f979c2394ade67435c
geekysilento <adityakarna.ak@gmail.com>
2024-02-02 21:04:15 +0545

    added more input generics

--- components/inputs/Visualizer.tsx
+++ components/inputs/Visualizer.tsx
@@ -3,13 +3,19 @@ import React from 'react';
 import { EmailField } from './email';
 import { NameField } from './name';
 import PhoneField from './telephone';
+import { DatePickerDemo } from './date';
+import { InputFile } from './fileUpload';
+import { CheckboxReactHookFormMultiple } from './select';
 
 export default function Visualizer() {
   return (
-    <div className="m-5 flex flex-col space-y-5">
+    <div className="m-5 flex flex-col space-y-8">
       <EmailField />
       <NameField />
       <PhoneField />
+      <DatePickerDemo />
+      <InputFile />
+      <CheckboxReactHookFormMultiple />
     </div>
   );
 }
