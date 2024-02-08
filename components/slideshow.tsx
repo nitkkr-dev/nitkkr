e8126e6328947e2c21453d81a6a417ba2f9da6aa
Siddhi <12114022@nitkkr.ac.in>
2024-02-08 11:24:11 +0530

    slideshow-changes

--- components/slideshow.tsx
+++ components/slideshow.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { useState } from 'react';
-export default function Slideshow (){
+export default function Slideshow() {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
 
   const slides: string[] = [
@@ -9,32 +9,45 @@ export default function Slideshow (){
   ];
 
   const prevSlide = (): void => {
-    setCurrentSlide((prev: number) => (prev === 0 ? slides.length - 1 : prev - 1));
+    setCurrentSlide((prev: number) =>
+      prev === 0 ? slides.length - 1 : prev - 1
+    );
   };
 
   const nextSlide = (): void => {
-    setCurrentSlide((prev: number) => (prev === slides.length - 1 ? 0 : prev + 1));
+    setCurrentSlide((prev: number) =>
+      prev === slides.length - 1 ? 0 : prev + 1
+    );
   };
 
   return (
     <div className="relative">
-      <button className="prev absolute top-1/2 transform -translate-y-1/2 opacity-0 transition-opacity duration-300 hover:opacity-100" onClick={prevSlide}>
+      <button
+        className="prev absolute top-1/2 transform -translate-y-1/2 opacity-0 transition-opacity duration-300 hover:opacity-100"
+        onClick={prevSlide}
+      >
         &#10094;
       </button>
-      <button className="next absolute top-1/2 transform -translate-y-1/2 opacity-0 transition-opacity duration-300 right-0 hover:opacity-100" onClick={nextSlide}>
+      <button
+        className="next absolute top-1/2 transform -translate-y-1/2 opacity-0 transition-opacity duration-300 right-0 hover:opacity-100"
+        onClick={nextSlide}
+      >
         &#10095;
       </button>
       {slides.map((slide: string, index: number) => (
-        <div className={index === currentSlide ? 'mySlides' :'mySlides hidden' } key={index}>
+        <div
+          className={`relative ${index === currentSlide ? '' : 'hidden'}`}
+          key={index}
+        >
           <Image
             src={slide}
             alt={`slides ${index + 1}`}
             width={1920}
             height={1080}
-            loading={index === 0 ? 'eager' : 'lazy'} 
+            loading={index === 0 ? 'eager' : 'lazy'}
           />
         </div>
       ))}
     </div>
   );
-};
+}
