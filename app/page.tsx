5c827de7af50d875def08258a12e85889b75ea31
GetPsyched <priyanshu@getpsyched.dev>
2024-02-08 17:09:59 +0530

    fix: font styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import DirectorsCorner from '@/app/directors-corner';
 
 export default function Home() {
   return (
-    <main className="bg-root font-regular">
+    <main>
       <DirectorsCorner />
     </main>
   );
