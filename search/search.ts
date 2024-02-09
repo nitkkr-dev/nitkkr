6b3e6c6a1ac5d173a0539629a56079d630c0f3f8
Aryamik Pandey <aryamikpandey@gmail.com>
2024-02-09 17:33:39 +0530

    perform search on all collections

---
+++ search/search.ts
@@ -0,0 +1,44 @@
+import Typesense, { Client } from 'typesense'
+import { SearchParams, SearchResponse } from 'typesense/lib/Typesense/Documents'
+
+const query = 'arya'
+
+const client = new Typesense.Client({
+    'nodes': [{
+        'host': 'localhost', // For Typesense Cloud use xxx.a1.typesense.net
+        'port': 8108,      // For Typesense Cloud use 443
+        'protocol': 'http'   // For Typesense Cloud use https
+    }],
+    'apiKey': 'xyz',
+    'connectionTimeoutSeconds': 2
+})
+
+interface CollectionMap {
+    [key: string]: string
+}
+
+const collections: string[] = [
+    'students',
+    'faculty',
+    'courses'
+]
+const collectionMap: CollectionMap = {
+    'students': 'name, institute_email',
+    'faculty': 'name',
+    'courses': 'title, code',
+}
+
+
+async function performSearch(client: Client, collectionMap: CollectionMap, collections: string[], query: string) {
+    let searchResults: SearchResponse<object>[] = []
+
+    for (let collection of collections) {
+        let searchParams: SearchParams = { 'q': query, 'query_by': collectionMap[collection] }
+
+        const searchResult = await client.collections(collection).documents().search(searchParams)
+        searchResults.push(searchResult)
+    }
+    console.log(searchResults)
+}
+
+performSearch(client, collectionMap, collections, query)
