6368ef85621af3ca7b51b390e1d702b3ae8fd734
GetPsyched <priyanshu@getpsyched.dev>
2024-02-11 15:23:10 +0530

    style: sort tailwind classes

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -14,28 +14,28 @@ const dmSerifDisplay = DM_Serif_Display({
 const NotificationButton = ({ category }: { category: string }) => (
   <button
     key={category}
-    className="cursor-pointer pt-[35px] px-[34px] pb-[34px] bg-neutral-50 self-stretch rounded-xl shadow-[6px_6px_40px_-5px_rgba(0,_0,_0,_0.25)] overflow-hidden flex flex-row items-center justify-center border-[1px] border-solid border-primary-red-700 hover:bg-gainsboro hover:box-border hover:border-[1px] hover:border-solid hover:border-tomato"
+    className="border-primary-red-700 hover:bg-gainsboro hover:border-tomato flex cursor-pointer flex-row items-center justify-center self-stretch overflow-hidden rounded-xl border-[1px] border-solid bg-neutral-50 px-[34px] pb-[34px] pt-[35px] shadow-[6px_6px_40px_-5px_rgba(0,_0,_0,_0.25)] hover:box-border hover:border-[1px] hover:border-solid"
   >
-    <div className="relative text-21xl leading-[48px] font-dm-serif-regular-display text-primary-red-700 text-center mq750:text-13xl mq750:leading-[38px] mq450:text-5xl mq450:leading-[29px]">
+    <div className="text-21xl font-dm-serif-regular-display text-primary-red-700 mq750:text-13xl mq750:leading-[38px] mq450:text-5xl mq450:leading-[29px] relative text-center leading-[48px]">
       {category}
     </div>
   </button>
 );
 
 const Notifications: NextPage = () => (
-  <div className="w-full relative h-[1059px] overflow-hidden bg-[url('/images/background-fight.png')] bg-cover bg-no-repeat bg-[top]">
-    <section className="absolute top-[275px] left-[194px] w-[1581px] flex flex-row items-start justify-start gap-[100px] max-w-full text-left text-base text-primary-red-700 font-dm-sans-bold-p mq1275:flex-wrap">
-      <div className="w-[470px] flex flex-col items-start justify-start gap-[43px] min-w-[470px] max-w-full mq750:min-w-full mq1275:flex-1">
+  <div className="relative h-[1059px] w-full overflow-hidden bg-[url('/images/background-fight.png')] bg-cover bg-[top] bg-no-repeat">
+    <section className="text-primary-red-700 font-dm-sans-bold-p mq1275:flex-wrap absolute left-[194px] top-[275px] flex w-[1581px] max-w-full flex-row items-start justify-start gap-[100px] text-left text-base">
+      <div className="mq750:min-w-full mq1275:flex-1 flex w-[470px] min-w-[470px] max-w-full flex-col items-start justify-start gap-[43px]">
         {['Academic', 'Tenders', 'Workshop', 'Recruitment'].map((category) => (
           <NotificationButton key={category} category={category} />
         ))}
       </div>
-      <div className="flex-1 rounded-xl bg-bg shadow-[0px_8px_0px_#e13f32_inset,_-12px_22px_60px_rgba(0,_43,_91,_0.15)] overflow-hidden flex flex-col items-center justify-start pt-[42px] px-[7px] pb-3 box-border gap-[1px] min-w-[667px] max-w-full z-[1] mq750:min-w-full">
-        <div className="self-stretch flex flex-row items-start justify-start py-0 pr-11 pl-5 box-border gap-[24px] max-w-full cursor-pointer">
-          <div className="flex-1 flex flex-col items-start justify-start gap-[20px] max-w-[calc(100%_-_31px)]">
-            <div className="self-stretch flex flex-row items-start justify-start gap-[12px] max-w-full">
+      <div className="bg-bg mq750:min-w-full z-[1] box-border flex min-w-[667px] max-w-full flex-1 flex-col items-center justify-start gap-[1px] overflow-hidden rounded-xl px-[7px] pb-3 pt-[42px] shadow-[0px_8px_0px_#e13f32_inset,_-12px_22px_60px_rgba(0,_43,_91,_0.15)]">
+        <div className="box-border flex max-w-full cursor-pointer flex-row items-start justify-start gap-[24px] self-stretch py-0 pl-5 pr-11">
+          <div className="flex max-w-[calc(100%_-_31px)] flex-1 flex-col items-start justify-start gap-[20px]">
+            <div className="flex max-w-full flex-row items-start justify-start gap-[12px] self-stretch">
               <img
-                className="h-6 w-6 relative"
+                className="relative h-6 w-6"
                 alt=""
                 src="/images/chevron-right.png"
               />
@@ -44,27 +44,27 @@ const Notifications: NextPage = () => (
                 Assistant (Ref.:Advt.No.03/2023)
               </span>
             </div>
-            <div className="self-stretch h-[0.5px] relative box-border border-t-[0.5px] border-solid border-darkslateblue" />
+            <div className="border-darkslateblue relative box-border h-[0.5px] self-stretch border-t-[0.5px] border-solid" />
           </div>
-          <div className="h-[486px] w-[7px] relative">
-            <div className="absolute top-[0px] left-[0px] rounded-81xl bg-primary-red-100 w-1.5 h-[486px] opacity-[0.2]" />
-            <div className="absolute top-[0px] left-[1px] rounded-81xl bg-primary-red-100 w-1.5 h-[238px] z-[1]" />
+          <div className="relative h-[486px] w-[7px]">
+            <div className="rounded-81xl bg-primary-red-100 absolute left-[0px] top-[0px] h-[486px] w-1.5 opacity-[0.2]" />
+            <div className="rounded-81xl bg-primary-red-100 absolute left-[1px] top-[0px] z-[1] h-[238px] w-1.5" />
           </div>
         </div>
-        <div className="w-[104.56px] rounded-2xl flex flex-row items-center justify-center py-3 px-5 box-border gap-[8px]">
-          <button className="flex-1 relative leading-[24px] overflow-hidden text-ellipsis whitespace-nowrap cursor-pointer text-[#e13f32] font-bold bg-transparent border-none outline-none">
+        <div className="box-border flex w-[104.56px] flex-row items-center justify-center gap-[8px] rounded-2xl px-5 py-3">
+          <button className="relative flex-1 cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap border-none bg-transparent font-bold leading-[24px] text-[#e13f32] outline-none">
             View All
           </button>
-          <img className="h-6 w-6 relative hidden min-h-[24px]" alt="" />
+          <img className="relative hidden h-6 min-h-[24px] w-6" alt="" />
         </div>
       </div>
     </section>
 
-    <div className="pt-20 items-center w-full h-fit mx-[242px] text-right flex flex-row">
+    <div className="mx-[242px] flex h-fit w-full flex-row items-center pt-20 text-right">
       <HorsesRunning direction="right" />
       <div
         className={
-          'text-red-700 text-[54px] font-bold flex ml-[10px] ' +
+          'ml-[10px] flex text-[54px] font-bold text-red-700 ' +
           dmSerifDisplay.className
         }
       >
