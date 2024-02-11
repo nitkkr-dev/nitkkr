26cdcf766856ebb236b368c17ea0c2a83b660f3c
GetPsyched <priyanshu@getpsyched.dev>
2024-02-12 04:07:24 +0530

    style: run formatter

--- middleware.ts
+++ middleware.ts
@@ -1,38 +1,39 @@
-import { headers as nextHeaders } from "next/headers";
-import { match } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
+import { match } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
+import { headers as nextHeaders } from 'next/headers';
 
-let locales: string[] = ["en-GB", "hi-IN"];
+let locales: string[] = ['en-GB', 'hi-IN'];
 
 function getLocale(request: any): string {
-    let languages = new Negotiator({
-        headers: {
-      "accept-language": nextHeaders().get("Accept-Language") as any
-    }}).languages();
-    
-    let defaultLocale = "en-GB";
+  let languages = new Negotiator({
+    headers: {
+      'accept-language': nextHeaders().get('Accept-Language') as any,
+    },
+  }).languages();
 
-    return match(languages, locales, defaultLocale);
+  let defaultLocale = 'en-GB';
+
+  return match(languages, locales, defaultLocale);
 }
 
 export function middleware(request: any) {
-    const { pathname } = request.nextUrl;
-    const pathnameHasLocale = locales.some(
-        (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-    );
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
 
-    if (pathnameHasLocale) return;
-    
-    const locale = getLocale(request);
-    request.nextUrl.pathname = `/${locale}${pathname}`;
+  if (pathnameHasLocale) return;
 
-    return Response.redirect(request.nextUrl);
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+
+  return Response.redirect(request.nextUrl);
 }
 
 export const config = {
   matcher: [
     // Skip all internal paths (_next)
-    "/((?!_next).*)",
+    '/((?!_next).*)',
     // Optional: only run on root (/) URL
     // '/'
   ],
