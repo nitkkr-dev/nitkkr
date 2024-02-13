68fa898f6176b7895d23fb56e288d5c4890a56a3
Saket Raman <raman.saket17@gmail.com>
2024-02-14 03:41:29 +0530

    run time error fix code

--- middleware.ts
+++ middleware.ts
@@ -108,4 +108,9 @@ export const config = {
       ],
     },
   ],
+  runtime: 'experimental-edge', // for Edge API Routes only
+  unstable_allowDynamic: [
+    '/lib/utilities.js', // allows a single file
+    '/node_modules/function-bind/**', // use a glob to allow anything in the function-bind 3rd party module
+  ],
 };
