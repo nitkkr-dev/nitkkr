b8774f395be4c8081a7789b02a7874126c0047aa
GetPsyched <priyanshu@getpsyched.dev>
2024-02-14 23:19:29 +0530

    refactor: minor cleanup

--- middleware.ts
+++ middleware.ts
@@ -19,55 +19,38 @@ function getPreferredLocale(request: NextRequest): string {
   return match(languages, locales, defaultLocale);
 }
 
-async function authCheck() {
+async function isAuthorised(requiredPermissions: string[]) {
   const session = await getServerSession(authOptions);
-
   if (!session) {
+    NextResponse.redirect('/login');
     return false;
-  } else return true;
-}
-
-async function roleCheck(requiredPermission: string[]) {
-  const session = await getServerSession(authOptions);
-  const user = session!.user!;
-  let hasRequiredRoles = false; // Initialize to false
-
-  if (user) {
-    const person = await prisma.persons.findFirst({
-      where: {
-        institute_email: user.email!,
-      },
-    });
-
-    const permissions = await prisma.auth_roles.findMany({
-      where: {
-        id: {
-          in: person?.role_ids || [],
-        },
-      },
-    });
-
-    const combinedPermissions = permissions.reduce((acc, curr) => {
-      return [...acc, ...curr.permissions];
-    }, [] as string[]);
-
-    hasRequiredRoles = requiredPermission.every((permission) =>
-      combinedPermissions.includes(permission)
-    );
-
-    return hasRequiredRoles;
   }
+
+  const person = await prisma.persons.findFirst({
+    where: { institute_email: session.user?.email! },
+  });
+
+  let permissions = await prisma.auth_roles.findMany({
+    where: { id: { in: person?.role_ids || [] } },
+  });
+
+  permissions = permissions.reduce(
+    (acc, curr) => [...acc, ...curr.permissions],
+    [] as string[]
+  );
+
+  return requiredPermissions.every((permission) =>
+    permissions.includes(permission)
+  );
 }
+
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/')) {
-    const { pathname } = request.nextUrl;
-    const pathnameHasLocale = locales.some(
-      (locale) =>
-        pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-    );
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
 
-    if (pathnameHasLocale) return;
-
+  if (!pathnameHasLocale) {
     const preferredLocale = getPreferredLocale(request);
     request.nextUrl.pathname = `/${preferredLocale}${pathname}`;
 
@@ -75,12 +58,8 @@ export async function middleware(request: NextRequest) {
   }
 
   if (request.nextUrl.pathname.includes('/student-profile')) {
-    const auth = await authCheck();
-    if (!auth) {
-      NextResponse.redirect('/login');
-    }
     const requiredRoles = ['student'];
-    const hasRequiredRoles = await roleCheck(requiredRoles);
+    const hasRequiredRoles = await isAuthorised(requiredRoles);
     if (!hasRequiredRoles) {
       NextResponse.redirect('/login');
     } else return NextResponse.next();
@@ -107,10 +86,5 @@ export const config = {
         { type: 'header', key: 'purpose', value: 'prefetch' },
       ],
     },
-  ],
-  runtime: 'experimental-edge', // for Edge API Routes only
-  unstable_allowDynamic: [
-    '/lib/utilities.js', // allows a single file
-    '/node_modules/function-bind/**', // use a glob to allow anything in the function-bind 3rd party module
   ],
 };
