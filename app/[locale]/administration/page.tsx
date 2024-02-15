171ace1bd3d62b2d10b01929919a98a00ff5a703
GetPsyched <priyanshu@getpsyched.dev>
2024-02-15 15:50:18 +0530

    fix: cleanup redundant pages

--- app/[locale]/administration/page.tsx
+++
@@ -1,9 +0,0 @@
-import WorkInProgress from '@/components/work-in-progress';
-
-export default function Administration({
-  params: { locale },
-}: {
-  params: { locale: string };
-}) {
-  return <WorkInProgress locale={locale} />;
-}
