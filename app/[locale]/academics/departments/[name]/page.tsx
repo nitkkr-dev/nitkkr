4f7c58b6eecb0b935d2b5f50c6d043199c68c106
KambojRajan <rajankamboj853@gmail.com>
2024-02-15 15:07:41 +0530

    "Department init(draft)"

--- app/[locale]/academics/departments/[name]/page.tsx
+++ app/[locale]/academics/departments/[name]/page.tsx
@@ -1,9 +1,40 @@
-import WorkInProgress from '@/components/work-in-progress';
+import AboutDisplay from '@/components/DepartmentRelated/aboutDisplay';
+import HodMessage from '@/components/DepartmentRelated/hodMessage';
+import MaxWidthWrapper from '@/components/DepartmentRelated/maxWidthWrapper';
+import MessionAndVission from '@/components/DepartmentRelated/messionAndVision';
+import SubNav from '@/components/DepartmentRelated/subNav';
+import HorsesRunning from '@/components/horses-running';
+import Image from 'next/image';
 
 export default function Department({
-  params: { locale },
+  params: { locale,name },
 }: {
   params: { locale: string; name: string };
 }) {
-  return <WorkInProgress locale={locale} />;
+  const departmentData = {
+    name: name.toUpperCase(),
+    titleImage:
+      'https://www.figma.com/file/2UXH7QR1dUT8KXgdGuYaC1/image/cf46d1812e6285efda1f8172b7fc3634c6d76130',
+  };
+  return (
+    <div>
+      <div className="relative flex h-[493px] w-screen flex-col items-center overflow-hidden">
+        <Image
+          src={departmentData.titleImage}
+          alt={departmentData.name}
+          fill
+          className="relative -z-20 object-cover"
+        />
+        <h1 className="mt-48 text-center text-5xl font-bold text-white">
+          {decodeURIComponent(departmentData.name)}
+        </h1>
+        <div className="mt-auto">
+          <SubNav />
+        </div>
+      </div>
+      <AboutDisplay />
+      <MessionAndVission />
+      <HodMessage />
+    </div>
+  )
 }
---
+++ components/DepartmentRelated/maxWidthWrapper.tsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx';
+import { ReactNode } from 'react';
+
+
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div
+      className={clsx(
+        'mx-auto w-full max-w-screen-xl px-2.5 md:px-20',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
