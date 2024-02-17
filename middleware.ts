9ab1ee5adb3feaddeaae947c95ff9cf07932132f
GetPsyched <priyanshu@getpsyched.dev>
2024-02-17 20:17:04 +0530

    fix: middleware would run on unwanted routes

--- middleware.ts
+++ middleware.ts
@@ -31,7 +31,19 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    // Skip all internal paths (_next)
-    '/((?!_next).*)',
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
   ],
 };
