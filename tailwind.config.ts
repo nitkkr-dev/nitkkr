3546d4a19c1c980fb0c9956f60bf143b922fe776
GetPsyched <priyanshu@getpsyched.dev>
2024-02-17 20:19:20 +0530

    fix: tailwind styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,40 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      background: '#F9F5EB',
+      neutral: {
+        50: '#FAFAFA',
+        100: '#F5F5F5',
+        200: '#DBDBDB',
+        300: '#C2C2C2',
+        400: '#A8A8A8',
+        500: '#8F8F8F',
+        600: '#757575',
+        700: '#424242',
+        800: '#292929',
+        900: '#101010',
+      },
+      primary: {
+        100: '#EE928B',
+        300: '#E7695F',
+        500: '#E13F32',
+        700: '#C5291D',
+        900: '#971F16',
+      },
+      secondary: {
+        100: '#004B9E',
+        300: '#003A7A',
+        500: '#002B5B',
+        700: '#001D3D',
+        900: '#000F1F',
+      },
+      shade: {
+        light: '#FFFFFF',
+        dark: '#000000',
+      },
+      transparent: 'transparent',
+    },
     container: {
       center: true,
       padding: {
@@ -22,39 +56,6 @@ const config: Config = {
       backgroundImage: {
         notifications:
           "linear-gradient(rgba(249, 245, 235, 0.6) 0%, rgba(249, 245, 235, 0.8) 85%, rgba(249, 245, 235, 1) 100%), url('https://s3-alpha-sig.figma.com/img/cff6/b90c/6d452f4caad45a22f4d4a701c7b48bce?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pXiNpSpLsmtFI9JXmo9h-cYFyP0m07hqsPbJfVVHdSguGKbK3NS4WOOK6-3vGv3IUYr6UllYZ8Wvp3uMKsBQo3rEbgF1Vi8uLAaaGRsBUQiejDJN-IlaB-nWytpj5wT~XNMptN~sh6SMqetjfRMZGo9Bp8fR-ogXQ-FsrrXdNV4SloUXFJ2TwfHKOECkz-ssaC5bjat1IleFWq0gh~EeJmL2mOjfpVtamUQk-OYhfr9arPB-bGqmeRVd9YNQbKr4zj5T5EGjxfSwdEihgoXflSqks5W1jvlPbnCEn-cCzzR1JpD3I~2nsDPatOaAAZa-cTra78ijTK83YITCDXqJNA__')",
-      },
-      colors: {
-        neutral: {
-          10: '#FAFAFA',
-          20: '#8F8F8F',
-          30: '#DBDBDB',
-          40: '#424242',
-          50: '#F5F5F5',
-          60: '#757575',
-          70: '#C2C2C2',
-          80: '#292929',
-          90: '#A8A8A8',
-          100: '#101010',
-        },
-        primary: {
-          0: '#EE928B',
-          10: '#E7695F',
-          20: '#C5291D',
-          30: '#E13F32',
-          40: '#971F16',
-        },
-        error: {
-          10: '#004B9E',
-          20: '#002B5B',
-          30: '#002B5B',
-          40: '#001D3D',
-          50: '#000F1F',
-        },
-        root: '#F9F5EB',
-        shades: {
-          10: '#FFFFFF',
-          20: '#000000',
-        },
       },
     },
     fontFamily: {
