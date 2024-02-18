c716643916a9f445a52294eeaa824f178a584de9
geekysilento <adityakarna.ak@gmail.com>
2024-02-18 16:14:56 +0545

    forwardRef-test

--- components/inputs/text.tsx
+++ components/inputs/text.tsx
@@ -1,22 +1,31 @@
 'use client';
 
 import { PiTextT } from 'react-icons/pi';
+import { forwardRef } from 'react';
 
 import { Input, InputProps } from '../ui/input';
 
-export const TextField = ({
-  label = 'Text',
-  placeholder = 'Enter your text',
-  LeftChild = PiTextT,
-  ...props
-}: InputProps) => {
+const TextField = forwardRef<HTMLInputElement, InputProps>(function TextField(
+  {
+    label = 'Text',
+    placeholder = 'Enter your text',
+    LeftChild = PiTextT,
+    ...props
+  },
+  ref
+) {
   return (
     <Input
+      ref={ref}
       type="text"
       placeholder={placeholder}
       label={label}
-      {...props}
       LeftChild={LeftChild}
+      {...props}
     />
   );
-};
+});
+
+TextField.displayName = 'TextField';
+
+export default TextField; // Export the component
