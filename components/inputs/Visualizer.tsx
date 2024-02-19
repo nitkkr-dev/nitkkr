eef688d117ad8e972f530bd5d1a0505614d48f3e
geekysilento <adityakarna.ak@gmail.com>
2024-02-20 00:31:09 +0545

    better select field and added htmlFor tags

--- components/inputs/Visualizer.tsx
+++ components/inputs/Visualizer.tsx
@@ -1,87 +1,103 @@
 'use client';
-
-import React from 'react';
+import React, { useRef } from 'react';
 
 import EmailField from './email';
 import TextField from './text';
 import PhoneField from './telephone';
 import DatePicker from './date';
 import FileUpload from './fileUpload';
-import { CheckboxReactHookFormMultiple } from './Checkbox';
+// import { CheckboxReactHookFormMultiple } from './Checkbox';
 import { DateTimePicker } from './date-time-picker/date-time-picker';
 import { TimeField } from './date-time-picker/time-field';
-import { SelectDropdown } from './selectIItem';
+import SelectDropdown from './selectIItem';
 
 export default function Visualizer() {
-  const items = [
-    {
-      id: 'recentsID',
-      label: 'RecentsLabel',
-    },
-    {
-      id: 'home',
-      label: 'Home',
-    },
-    {
-      id: 'applications',
-      label: 'Applications',
-    },
-    {
-      id: 'desktop123',
-      label: 'Desktop123',
-    },
-    {
-      id: 'downloads',
-      label: 'Downloads',
-    },
-    {
-      id: 'documents',
-      label: 'Documents',
-    },
-  ];
+  const emailRef = useRef(null);
+  const nameRef = useRef(null);
+  const dateRef = useRef(null);
+  const fileRef = useRef(null);
+  // const checkboxRef = useRef(null);
+  const phoneRef = useRef(null);
+  const dateTimeRef = useRef(null);
+  const timeRef = useRef(null);
+  const selectRef = useRef(null);
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    // const formData = {
+    //   email: emailRef.current.value,
+    //   name: nameRef.current.value,
+    //   date: dateRef.current.value,
+    //   file: fileRef.current.files[0],
+    //   // checkbox: checkboxRef.current.value,
+    //   phone: phoneRef.current.value,
+    //   dateTime: dateTimeRef.current.value,
+    //   // time: timeRef.current.value,
+    //   select: selectRef.current.value,
+    // };
+    console.log('Submitted Data:');
+  };
+
+  const items = ['item1', 'item2', 'item3', 'item4'];
+
   return (
-    <div className="w-full space-y-6">
-      <EmailField
-        description="email field hai ye"
-        required
-        errorMsg="this is a error msg"
-      />
-      <TextField required label="Name" placeholder="Enter your name" />
-      <DatePicker
-        required
-        description="this is a description"
-        errorMsg="mooj kro"
-      />
-      <FileUpload required accept=".ppt" />
-      <CheckboxReactHookFormMultiple
-        items={items}
-        description="check only one value"
-        selectionMode="multi"
-        required
-        errorMsg="valo"
-      />
-      <PhoneField
-        description="this is phone number"
-        errorMsg="acha yele error khale"
-      />
-      <DateTimePicker
-        granularity={'minute'}
-        description={'wrote'}
-        required
-        errorMsg="error "
-      />
-      <TimeField
-        label="babababa"
-        required
-        description="abcd"
-        errorMsg="acha error le"
-      />
-      <SelectDropdown
-        items={items}
-        required
-        description="select karo"
-        errorMsg="acha error le"
-      />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="w-full space-y-6">
+        <EmailField
+          ref={emailRef}
+          description="email field hai ye"
+          required
+          errorMsg="this is a error msg"
+        />
+        <TextField
+          ref={nameRef}
+          required
+          label="Name"
+          placeholder="Enter your name"
+        />
+        <DatePicker
+          ref={dateRef}
+          required
+          description="this is a description"
+          errorMsg="mooj kro"
+        />
+        <FileUpload ref={fileRef} required accept=".ppt" />
+        {/* <CheckboxReactHookFormMultiple
+          ref={checkboxRef}
+          items={items}
+          description="check only one value"
+          selectionMode="multi"
+          required
+          errorMsg="valo"
+        /> */}
+        <PhoneField
+          ref={phoneRef}
+          description="this is phone number"
+          errorMsg="acha yele error khale"
+        />
+        <DateTimePicker
+          ref={dateTimeRef}
+          granularity={'minute'}
+          description={'wrote'}
+          required
+          errorMsg="error "
+        />
+        <TimeField
+          label="babababa"
+          required
+          description="abcd"
+          errorMsg="acha error le"
+        />
+        <SelectDropdown
+          ref={selectRef}
+          items={items}
+          required
+          description="select karo"
+          errorMsg="acha error le"
+          defaultValue={items[1]}
+        />
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   );
 }
