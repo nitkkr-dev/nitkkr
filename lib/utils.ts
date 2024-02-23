5fd4a3cbf8e311b7a035c02fc9f89f9aad72a08e
GetPsyched <priyanshu@getpsyched.dev>
2024-02-24 00:03:43 +0530

    feat: init shadcn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function getKeys<T extends object>(obj: T): (keyof T)[] {
   return Object.keys(obj) as (keyof T)[];
 }
