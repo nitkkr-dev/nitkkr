e82c550b80500b8a5c6fde85edcf4835cc167481
GetPsyched <priyanshu@getpsyched.dev>
2024-02-23 22:36:07 +0530

    fix: we won't be keeping any migrations until prod db is setup

--- prisma/migrations/20240203115546_added_new_feild_in_person/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "courses" DROP CONSTRAINT "fk_courses_majors";
-
--- AlterTable
-ALTER TABLE "majors" ADD COLUMN     "coursesId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "majors" ADD CONSTRAINT "majors_coursesId_fkey" FOREIGN KEY ("coursesId") REFERENCES "courses"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20240203115723_added_role_ids/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "persons" ADD COLUMN     "role_ids" SMALLINT[];
