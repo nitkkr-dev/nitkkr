66493408bbbeec651ff16b2f8319f36efc748105
GetPsyched <priyanshu@getpsyched.dev>
2024-02-24 09:51:39 +0530

    fix: 600ms delay feels laggy

--- components/scroll-area.tsx
+++ components/scroll-area.tsx
@@ -12,6 +12,7 @@ const ScrollArea = React.forwardRef<
   <ScrollAreaPrimitive.Root
     ref={ref}
     className={cn('relative overflow-hidden', className)}
+    scrollHideDelay={0}
     {...props}
   >
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
