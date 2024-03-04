e40270ded858023abe6f56eee23377591a4002eb
som-04 <somyasati14@gmail.com>
2024-03-04 17:02:44 +0300

    Carousel now made with shadcn buttons restored to the original position. working on adding the arrowkeys logic now.

--- components/slideshow.tsx
+++ components/slideshow.tsx
@@ -1,80 +1,67 @@
 'use client';
 
+import type { EmblaOptionsType } from 'embla-carousel';
+import Autoplay from 'embla-carousel-autoplay';
 import Image from 'next/image';
-import { useRef } from 'react';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick-theme.css';
-import 'slick-carousel/slick/slick.css';
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '~/components/ui/carousel';
 
 export default function Slideshow({
   images,
 }: {
   images: { image: string; title?: string; subtitle?: string }[];
 }) {
-  const sliderRef = useRef<Slider | null>(null);
-
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 7000,
-    arrows: false,
-  };
-
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if (event.key === 'ArrowLeft') {
-      sliderRef.current?.slickPrev();
-    } else if (event.key === 'ArrowRight') {
-      sliderRef.current?.slickNext();
-    }
-  };
+  const options: EmblaOptionsType = { loop: true, align: 'center' };
 
   return (
-    <article
-      className="relative overflow-x-hidden"
-      onMouseEnter={() => window.addEventListener('keydown', handleKeyDown)}
-      onMouseLeave={() => window.removeEventListener('keydown', handleKeyDown)}
-    >
-      <Slider ref={sliderRef} {...settings}>
-        {images.map(({ image, title, subtitle }, index) => (
-          <figure className="relative max-h-screen" key={index}>
-            <Image
-              alt={`slide ${index + 1}`}
-              className="h-full"
-              height={1080}
-              loading={index === 0 ? 'eager' : 'lazy'}
-              width={1920}
-              src={image}
-            />
-            {title && (
-              <figcaption className="container absolute inset-x-0 bottom-0 min-w-full bg-gradient-to-b from-transparent to-neutral-800 py-6">
-                <h4 className="text-neutral-100">{title}</h4>
-                <p className="text-neutral-100">{subtitle}</p>
-              </figcaption>
-            )}
-          </figure>
-        ))}
-      </Slider>
+    <article className="relative overflow-x-hidden">
+      <Carousel
+        className="w-full"
+        opts={options}
+        plugins={[
+          Autoplay({
+            delay: 7000,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          {images.map(({ image, title, subtitle }, index) => (
+            <CarouselItem key={index}>
+              <figure className="relative max-h-screen">
+                <Image
+                  alt={`slide ${index + 1}`}
+                  className="h-full"
+                  height={1080}
+                  loading={index === 0 ? 'eager' : 'lazy'}
+                  width={1920}
+                  src={image}
+                />
+                {title && (
+                  <figcaption className="container absolute inset-x-0 bottom-0 min-w-full bg-gradient-to-b from-transparent to-neutral-800 py-6">
+                    <h4 className="text-neutral-100">{title}</h4>
+                    <p className="text-neutral-100">{subtitle}</p>
+                  </figcaption>
+                )}
+              </figure>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <section className="container absolute inset-0 flex min-w-full justify-between">
+          <button className="opacity-60 hover:opacity-100">
+            <CarouselPrevious />
+          </button>
 
-      <section className="container absolute inset-0 flex min-w-full justify-between">
-        <button
-          className="opacity-60 hover:opacity-100"
-          onClick={() => sliderRef.current?.slickPrev()}
-        >
-          <FaChevronLeft className="text-neutral-100" size={40} />
-        </button>
-
-        <button
-          className="opacity-60 hover:opacity-100"
-          onClick={() => sliderRef.current?.slickNext()}
-        >
-          <FaChevronRight className="text-neutral-100" size={40} />
-        </button>
-      </section>
+          <button className="opacity-60 hover:opacity-100">
+            <CarouselNext />
+          </button>
+        </section>
+      </Carousel>
     </article>
   );
 }
