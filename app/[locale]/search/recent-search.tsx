abdc82af8e3b875231df394002f439aa2f611775
Antriksh Kumar <68548165+heydoyouknowme0@users.noreply.github.com>
2024-03-07 20:05:55 +0000

    feat: added search load and recent searches

---
+++ app/[locale]/search/recent-search.tsx
@@ -0,0 +1,43 @@
+'use client';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export default function Recents() {
+  const [recents, setRecents] = useState<{ value: string; label: string }[]>(
+    JSON.parse(localStorage.getItem('recentSearches') ?? '[]') as {
+      value: string;
+      label: string;
+    }[]
+  );
+
+  const clearRecents = () => {
+    localStorage.removeItem('recentSearches');
+    setRecents([]);
+  };
+  return (
+    <section className="mt-12">
+      <header className="flex">
+        <h5 className="grow">Recent Searches</h5>
+        <button
+          className="mb-0 max-h-fit rounded-md  p-2 align-baseline  hover:bg-primary-300"
+          onClick={clearRecents}
+        >
+          Clear recents
+        </button>
+      </header>
+
+      <ol className="flex gap-6">
+        {recents.map(
+          (
+            { label, value }: { label: string; value: string },
+            index: number
+          ) => (
+            <li key={index}>
+              <Link href={value}>{label}</Link>
+            </li>
+          )
+        )}
+      </ol>
+    </section>
+  );
+}
---
+++ app/[locale]/search/searchbar.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useDebounceCallback } from 'usehooks-ts';
+
+export default function Searchbar() {
+  const router = useRouter();
+
+  return (
+    <input
+      className="min-w-full rounded-lg border border-primary-700 px-4 py-2 focus:outline-none"
+      placeholder="Quick Search..."
+      onChange={useDebounceCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+          router.replace(`?q=${e.target.value}`);
+        },
+        300
+      )}
+    />
+  );
+}
