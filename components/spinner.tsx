3f0aca534ed53ce8f2698e17780c27ba97ed3160
geekysilento <adityakarna.ak@gmail.com>
2024-03-16 00:44:47 +0545

    feat:  notification content skeletons

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import { LuLoader } from 'react-icons/lu';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 
 import { Skeleton } from './ui/skeleton';
 
-export const Spinner = () => {
-  return (
-    <div className="flex h-[90%] w-full items-center justify-center md:h-[91%] lg:h-[87%] xl:h-[85%]">
-      <LuLoader className="text-red-500 h-8 w-8 animate-spin" />
-    </div>
-  );
-};
-
 const MyLoader = () => (
   <>
-    <div className="my-2 flex max-w-full sm:my-4 xl:my-5">
-      <MdOutlineKeyboardArrowRight className="my-auto size-4 text-neutral-300 lg:size-6" />
-      <Skeleton className="h-[20px] w-full rounded-md" />
+    <div className="h-auto">
+      {Array.from({ length: 6 }).map((_, index) => (
+        <div key={index} className="my-2 w-full max-w-full  sm:my-4 xl:my-5">
+          <div className="flex w-full items-center justify-center">
+            <MdOutlineKeyboardArrowRight className="my-auto size-4 text-neutral-300 lg:size-6" />
+            <Skeleton className="h-[20px] w-full rounded-md" />
+          </div>
+          <hr className="mt-5 opacity-20" />
+        </div>
+      ))}
     </div>
-    <hr className="opacity-20" />
   </>
 );
 
