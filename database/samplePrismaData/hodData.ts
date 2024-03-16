0533e8856ff226f0a27f84205a4279d9c462775e
aryansri-19 <aryansri.19@gmail.com>
2024-03-16 13:53:57 +0530

    Data defined for setting up db and db populating functions

---
+++ database/samplePrismaData/hodData.ts
@@ -0,0 +1,63 @@
+export const hodData = [
+  {
+    faculty_id: 1,
+    department_id: 1,
+    message: 'Welcome to the Department of Business Administration',
+    is_active: false,
+  },
+  {
+    faculty_id: 2,
+    department_id: 2,
+    message: 'Welcome to the Department of Chemistry',
+    is_active: true,
+  },
+  {
+    faculty_id: 3,
+    department_id: 3,
+    message: 'Welcome to the Department of Civil Engineering',
+    is_active: true,
+  },
+  {
+    faculty_id: 4,
+    department_id: 4,
+    message: 'Welcome to the Department of Computer Engineering',
+    is_active: true,
+  },
+  {
+    faculty_id: 5,
+    department_id: 5,
+    message: 'Welcome to the Department of Electrical Engineering',
+    is_active: true,
+  },
+  {
+    faculty_id: 6,
+    department_id: 6,
+    message:
+      'Welcome to the Department of Electronics & Communication Engineering',
+    is_active: true,
+  },
+  {
+    faculty_id: 7,
+    department_id: 7,
+    message: 'Welcome to the Department of Humanities & Social Sciences',
+    is_active: true,
+  },
+  {
+    faculty_id: 8,
+    department_id: 8,
+    message: 'Welcome to the Department of Mathematics',
+    is_active: true,
+  },
+  {
+    faculty_id: 9,
+    department_id: 9,
+    message: 'Welcome to the Department of Mechanical Engineering',
+    is_active: true,
+  },
+  {
+    faculty_id: 10,
+    department_id: 10,
+    message: 'Welcome to the Department of Physics',
+    is_active: true,
+  },
+];
---
+++ database/samplePrismaData/personsData.ts
@@ -0,0 +1,12 @@
+export const personsData = [
+  { name: 'John Doe', institute_email: 'example@nitkkr.ac.in' },
+  { name: 'Alice Smith', institute_email: 'alice.smith@nitkkr.ac.in' },
+  { name: 'Bob Johnson', institute_email: 'bob.johnson@nitkkr.ac.in' },
+  { name: 'Emma Brown', institute_email: 'emma.brown@nitkkr.ac.in' },
+  { name: 'William Davis', institute_email: 'william.davis@nitkkr.ac.in' },
+  { name: 'Olivia Wilson', institute_email: 'olivia.wilson@nitkkr.ac.in' },
+  { name: 'James Taylor', institute_email: 'james.taylor@nitkkr.ac.in' },
+  { name: 'Sophia Martinez', institute_email: 'sophia.martinez@nitkkr.ac.in' },
+  { name: 'Ethan Anderson', institute_email: 'ethan.anderson@nitkkr.ac.in' },
+  { name: 'Grace Lee', institute_email: 'grace.lee@nitkkr.ac.in' },
+];
