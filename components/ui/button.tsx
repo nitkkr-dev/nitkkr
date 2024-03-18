411d0e66436af01f20c4320bc4d07a4132a31e6a
GetPsyched <priyanshu@getpsyched.dev>
2024-03-19 00:42:43 +0530

    style: add custom styling to the button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,33 +5,41 @@ import * as React from 'react';
 import { cn } from '~/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300',
+  cn(
+    'inline-flex items-center justify-center whitespace-nowrap transition-colors',
+    'ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-900 focus-visible:ring-offset-2',
+    'rounded sm:rounded-lg lg:rounded-xl'
+  ),
   {
     variants: {
       variant: {
-        default:
-          'bg-gray-900 text-gray-50 hover:bg-gray-900/90 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90',
-        destructive:
-          'bg-red-500 text-gray-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-gray-50 dark:hover:bg-red-900/90',
-        outline:
-          'border border-gray-200 bg-white hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50',
-        secondary:
-          'bg-gray-100 text-gray-900 hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80',
-        ghost:
-          'hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50',
-        link: 'text-gray-900 underline-offset-4 hover:underline dark:text-gray-50',
-      },
-      size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        default: cn(
+          'bg-primary-500 text-neutral-50',
+          'hover:bg-primary-700',
+          'focus:bg-primary-900',
+          'active:bg-primary-900'
+        ),
+        outline: cn(
+          'border border-primary-500 text-primary-500',
+          'hover:border-primary-700 hover:text-primary-700',
+          'focus:border-primary-900 focus:text-primary-900',
+          'active:border-primary-900 active:text-primary-900'
+        ),
+        ghost: cn(
+          'text-primary-500',
+          'hover:bg-primary-500 hover:text-neutral-50',
+          'focus:bg-primary-700 focus:text-neutral-50',
+          'active:bg-primary-700 active:text-neutral-50'
+        ),
+        link: cn(
+          'text-primary-500 underline-offset-4 hover:underline',
+          'hover:text-primary-700',
+          'focus:text-primary-900',
+          'active:text-primary-900'
+        ),
       },
     },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default',
-    },
+    defaultVariants: { variant: 'default' },
   }
 );
 
@@ -42,11 +50,11 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, className }))}
         ref={ref}
         {...props}
       />
