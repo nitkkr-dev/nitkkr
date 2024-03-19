918c1eef6a967edb150bcf2868b092707a453b72
geekysilento <adityakarna.ak@gmail.com>
2024-03-20 00:45:38 +0545

    fix: signin button directly routes to API

---
+++ app/[locale]/login/loginbutton.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { signIn } from 'next-auth/react';
+import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+
+const LoginButton = ({ logintext }: { logintext: string }) => {
+  return (
+    <button
+      onClick={() => signIn('google', { callbackUrl: '/' })}
+      className="text-md mt-5 flex w-full items-center justify-center space-x-3 rounded-md border border-primary-500 bg-shade-light p-4 text-primary-700 hover:bg-neutral-100"
+    >
+      <FcGoogle className="h-6 w-6" />
+      <span>{logintext}</span>
+    </button>
+  );
+};
+
+export default LoginButton;
