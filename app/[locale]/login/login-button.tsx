5de2988f3eb25b9dc7179fafdd580e6d60a36343
geekysilento <adityakarna.ak@gmail.com>
2024-03-20 13:25:57 +0545

    fix: file casing fixes

--- app/[locale]/login/login-button.tsx
+++ app/[locale]/login/login-button.tsx
@@ -1,16 +1,17 @@
 'use client';
+
 import { signIn } from 'next-auth/react';
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 
-const LoginButton = ({ logintext }: { logintext: string }) => {
+const LoginButton = ({ loginText }: { loginText: string }) => {
   return (
     <button
       onClick={() => signIn('google', { callbackUrl: '/' })}
       className="text-md mt-5 flex w-full items-center justify-center space-x-3 rounded-md border border-primary-500 bg-shade-light p-4 text-primary-700 hover:bg-neutral-100"
     >
       <FcGoogle className="h-6 w-6" />
-      <span>{logintext}</span>
+      {loginText}
     </button>
   );
 };
