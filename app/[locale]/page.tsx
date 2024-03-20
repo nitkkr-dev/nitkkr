5c78f64c114bac14bee3f1bcfb3a40ebfab4e54c
Somya <somyasati14@gmail.com>
2024-03-20 12:28:32 +0300

    feat: slideshow (#88)
    Fixes #16
    
    - Image changes every 7 seconds.
    - Arrow keys can be used to change images.
    - Sliding animation included.
    
    ---------
    
    Co-authored-by: GetPsyched <priyanshu@getpsyched.dev>

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,6 @@
-import DirectorsCorner from './directors-corner';
-import Notifications from './notifications';
+import DirectorsCorner from '~/app/directors-corner';
+import Notifications from '~/app/notifications';
+import Slideshow from '~/components/slideshow';
 
 export default function Home({
   params: { locale },
@@ -16,6 +17,25 @@ export default function Home({
 }) {
   return (
     <main className="w-full">
+      <Slideshow
+        images={[
+          {
+            image:
+              'https://nitkkr.ac.in/wp-content/uploads/2023/11/IMG20220903190255-1-scaled.jpg',
+            title: 'NIT KKR deemed the First Ever NIT With All Green Campus!',
+            subtitle:
+              'Over 900 Acres of green foliage planted alongside the campus walls, the campus of the esteemed...',
+          },
+          {
+            image:
+              'https://nitkkr.ac.in/wp-content/uploads/2022/01/24131961_285405678647849_426967072086000359_o.jpg',
+          },
+          {
+            image:
+              'https://nitkkr.ac.in/wp-content/uploads/2022/12/IMG_0496-scaled.jpg',
+          },
+        ]}
+      />
       <Notifications category={notificationCategory} locale={locale} />
       <DirectorsCorner locale={locale} />
     </main>
---
+++ components/slideshow.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import type { EmblaOptionsType } from 'embla-carousel';
+import Autoplay from 'embla-carousel-autoplay';
+import Image from 'next/image';
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '~/components/ui';
+
+export default function Slideshow({
+  images,
+}: {
+  images: { image: string; title?: string; subtitle?: string }[];
+}) {
+  const options: EmblaOptionsType = { loop: true, align: 'center' };
+
+  return (
+    <article className="relative overflow-x-hidden">
+      <Carousel
+        className="w-full"
+        opts={options}
+        onHoverKeyboardControls
+        plugins={[Autoplay({ delay: 7000 })]}
+      >
+        <CarouselContent>
+          {images.map(({ image, title, subtitle }, index) => (
+            <CarouselItem key={index} className="relative max-h-screen">
+              <figure className="h-full">
+                <Image
+                  alt={`slide ${index + 1}`}
+                  className="h-full w-full object-cover"
+                  height={1080}
+                  loading={index === 0 ? 'eager' : 'lazy'}
+                  width={1920}
+                  src={image}
+                />
+                {title && (
+                  <figcaption className="absolute inset-x-0 bottom-0 min-w-full bg-gradient-to-b from-transparent to-neutral-800 py-6">
+                    <article className="container">
+                      <h4 className="pl-2 text-neutral-100">{title}</h4>
+                      <p className="pl-2 text-neutral-100">{subtitle}</p>
+                    </article>
+                  </figcaption>
+                )}
+              </figure>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+
+        <section className="container absolute inset-0 top-[50%] flex h-0 justify-between">
+          <CarouselPrevious className="static opacity-60 hover:opacity-100 focus-visible:ring-0 focus-visible:ring-offset-0" />
+          <CarouselNext className="static opacity-60 hover:opacity-100 focus-visible:ring-0 focus-visible:ring-offset-0" />
+        </section>
+      </Carousel>
+    </article>
+  );
+}
