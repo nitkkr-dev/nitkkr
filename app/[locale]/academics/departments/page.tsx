c30938030996bea621df71eff350f5f834bc5cfc
KambojRajan <rajankamboj853@gmail.com>
2024-03-25 18:18:51 +0530

    fix: spacing mistakes

--- app/[locale]/academics/departments/page.tsx
+++ app/[locale]/academics/departments/page.tsx
@@ -1,39 +1,37 @@
 import Link from 'next/link';
-import { AiOutlineCalculator } from 'react-icons/ai';
-import {
-  FaBriefcase,
-  FaBroadcastTower,
-  FaGlobeAfrica,
-  FaLaptop,
-  FaLeaf,
-} from 'react-icons/fa';
+import { BiLeaf } from 'react-icons/bi';
+import { CgLaptop } from 'react-icons/cg';
 import { FaGears } from 'react-icons/fa6';
+import { FaGlobeAfrica } from 'react-icons/fa';
 import { ImLab } from 'react-icons/im';
 import {
+  MdCellTower,
   MdDeveloperBoard,
   MdElectricalServices,
   MdHouseSiding,
   MdOutlineSolarPower,
 } from 'react-icons/md';
+import { RiBriefcase4Line } from 'react-icons/ri';
 import { SiAdobedreamweaver } from 'react-icons/si';
+import { SlCalculator } from 'react-icons/sl';
 
 import Heading from '~/components/heading';
 import { getTranslations } from '~/i18n/translations';
 
 const DEPARTMENTS = [
-  <FaBriefcase key="briefcase" className="h-6 w-6" />,
-  <MdDeveloperBoard key="developerBoard" className="h-6 w-6" />,
-  <ImLab key="lab" className="h-6 w-6" />,
-  <MdHouseSiding key="houseSiding" className="h-6 w-6" />,
-  <FaLaptop key="laptop" className="h-6 w-6" />,
-  <MdElectricalServices key="electricalServices" className="h-6 w-6" />,
-  <FaBroadcastTower key="broadcastTower" className="h-6 w-6" />,
-  <FaLeaf key="leaf" className="h-6 w-6" />,
-  <AiOutlineCalculator key="calculator" className="h-6 w-6" />,
-  <FaGears key="gears" className="h-6 w-6" />,
-  <FaGlobeAfrica key="globeAfrica" className="h-6 w-6" />,
-  <MdOutlineSolarPower key="solarPanel" className="h-6 w-6" />,
-  <SiAdobedreamweaver key="dreamweaver" className="h-6 w-6" />,
+  <RiBriefcase4Line key="briefcase" className="h-8 w-8" />,
+  <MdDeveloperBoard key="developerBoard" className="h-8 w-8" />,
+  <ImLab key="lab" className="h-8 w-8" />,
+  <MdHouseSiding key="houseSiding" className="h-8 w-8" />,
+  <CgLaptop key="laptop" className="h-8 w-8" />,
+  <MdElectricalServices key="electricalServices" className="h-8 w-8" />,
+  <MdCellTower key="broadcastTower" className="h-8 w-8" />,
+  <BiLeaf key="leaf" className="h-8 w-8" />,
+  <SlCalculator key="calculator" className="h-8 w-8" />,
+  <FaGears key="gears" className="h-8 w-8" />,
+  <FaGlobeAfrica key="globeAfrica" className="h-8 w-8" />,
+  <MdOutlineSolarPower key="solarPanel" className="h-8 w-8" />,
+  <SiAdobedreamweaver key="dreamweaver" className="h-8 w-8" />,
 ];
 
 export default async function Departments({
@@ -44,26 +42,26 @@ export default async function Departments({
   const text = (await getTranslations(locale)).Departments;
 
   return (
-    <main className="container mb-5 mt-20 flex flex-col items-center">
-      <Heading className="flex" glyphDirection={'dual'} href={''}>
-        <h1 className="mt-2 text-primary-700"> DEPARTMENTS</h1>
+    <main className="container mb-24 mt-24 items-center  p-0">
+      <Heading glyphDirection={'dual'} href={''}>
+        <h1 className="text-primary-700">DEPARTMENTS</h1>
       </Heading>
-      <div className="grid grid-cols-1 gap-y-7 px-2 sm:grid-cols-2 sm:gap-x-14 md:grid-cols-3 lg:grid-cols-4">
-        {DEPARTMENTS.map((dept, i) => (
+      <section className="grid flex-1 grid-cols-4 gap-x-16 gap-y-10">
+        {text.departmentTitles.map((item, i) => (
           <Link
-            key={i}
-            href={`/${locale}/academics/departments/${text.departmentTitles[i]}`}
-            className="group flex h-16 max-w-48 items-center space-x-3 rounded-md bg-neutral-50 px-1 py-2 shadow-lg hover:bg-primary-700"
+            href={`/${locale}/academics/departments/${item}`}
+            key={item}
+            className="group flex h-24 items-center space-x-4 rounded-md bg-neutral-50 px-2 py-1 shadow-md hover:bg-primary-700"
           >
-            <div className="flex-shrink-0 rounded-md bg-primary-700 p-2 text-background group-hover:bg-background group-hover:text-primary-700">
-              {dept}
+            <div className="rounded-md bg-primary-700 p-4 text-background group-hover:bg-background group-hover:text-primary-700">
+              {DEPARTMENTS[i]}
             </div>
-            <h5 className="text-sm text-primary-700 group-hover:text-background">
-              {text.departmentTitles[i].toUpperCase()}
-            </h5>
+            <h6 className="text-primary-700 group-hover:text-background">
+              {text.departmentTitles[i]}
+            </h6>
           </Link>
         ))}
-      </div>
+      </section>
     </main>
   );
 }
