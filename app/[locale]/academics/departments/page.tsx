f4f56a50559080b7b008448a57f9c5208a4331cc
KambojRajan <rajankamboj853@gmail.com>
2024-03-26 08:52:59 +0530

    fix: departments spacing

--- app/[locale]/academics/departments/page.tsx
+++ app/[locale]/academics/departments/page.tsx
@@ -15,23 +15,24 @@ import { RiBriefcase4Line } from 'react-icons/ri';
 import { SiAdobedreamweaver } from 'react-icons/si';
 import { SlCalculator } from 'react-icons/sl';
 
+import { Card, CardContent } from '~/components/ui/card';
 import Heading from '~/components/heading';
 import { getTranslations } from '~/i18n/translations';
 
 const DEPARTMENTS = [
-  <RiBriefcase4Line key="briefcase" className="h-8 w-8" />,
-  <MdDeveloperBoard key="developerBoard" className="h-8 w-8" />,
-  <ImLab key="lab" className="h-8 w-8" />,
-  <MdHouseSiding key="houseSiding" className="h-8 w-8" />,
-  <CgLaptop key="laptop" className="h-8 w-8" />,
-  <MdElectricalServices key="electricalServices" className="h-8 w-8" />,
-  <MdCellTower key="broadcastTower" className="h-8 w-8" />,
-  <BiLeaf key="leaf" className="h-8 w-8" />,
-  <SlCalculator key="calculator" className="h-8 w-8" />,
-  <FaGears key="gears" className="h-8 w-8" />,
-  <FaGlobeAfrica key="globeAfrica" className="h-8 w-8" />,
-  <MdOutlineSolarPower key="solarPanel" className="h-8 w-8" />,
-  <SiAdobedreamweaver key="dreamweaver" className="h-8 w-8" />,
+  <RiBriefcase4Line key="briefcase" className="h-7 w-7" />,
+  <MdDeveloperBoard key="developerBoard" className="h-7 w-7" />,
+  <ImLab key="lab" className="h-7 w-7" />,
+  <MdHouseSiding key="houseSiding" className="h-7 w-7" />,
+  <CgLaptop key="laptop" className="h-7 w-7" />,
+  <MdElectricalServices key="electricalServices" className="h-7 w-7" />,
+  <MdCellTower key="broadcastTower" className="h-7 w-7" />,
+  <BiLeaf key="leaf" className="h-7 w-7" />,
+  <SlCalculator key="calculator" className="h-7 w-7" />,
+  <FaGears key="gears" className="h-7 w-7" />,
+  <FaGlobeAfrica key="globeAfrica" className="h-7 w-7" />,
+  <MdOutlineSolarPower key="solarPanel" className="h-7 w-7" />,
+  <SiAdobedreamweaver key="dreamweaver" className="h-7 w-7" />,
 ];
 
 export default async function Departments({
@@ -42,23 +43,23 @@ export default async function Departments({
   const text = (await getTranslations(locale)).Departments;
 
   return (
-    <main className="container mb-24 mt-24 items-center  p-0">
+    <main className="border- container mb-24 mt-24 flex w-full flex-col items-center border-primary-700 ">
       <Heading glyphDirection={'dual'} href={''}>
-        <h1 className="text-primary-700">DEPARTMENTS</h1>
+        <h3 className="text-primary-700">DEPARTMENTS</h3>
       </Heading>
-      <section className="grid flex-1 grid-cols-4 gap-x-16 gap-y-10">
+      <section className="mx-auto grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {text.departmentTitles.map((item, i) => (
-          <Link
-            href={`/${locale}/academics/departments/${item}`}
-            key={item}
-            className="group flex h-24 items-center space-x-4 rounded-md bg-neutral-50 px-2 py-1 shadow-md hover:bg-primary-700"
-          >
-            <div className="rounded-md bg-primary-700 p-4 text-background group-hover:bg-background group-hover:text-primary-700">
-              {DEPARTMENTS[i]}
+          <Link href={`/${locale}/academics/departments/${item}`} key={item}>
+            <div className="bg-red-500 h-[85px] w-[250px] rounded-md border-none bg-neutral-50 shadow-md hover:bg-primary-700 ">
+              <div className="group flex h-full items-center space-x-4 rounded-md bg-neutral-50 p-2 hover:bg-primary-700 ">
+                <div className="bg-gray-800 ml-2.5 rounded-md bg-primary-700 p-2 text-background group-hover:bg-background group-hover:text-primary-700">
+                  {DEPARTMENTS[i]}
+                </div>
+                <h6 className="text-primary-700 group-hover:text-background">
+                  {text.departmentTitles[i]}
+                </h6>
+              </div>
             </div>
-            <h6 className="text-primary-700 group-hover:text-background">
-              {text.departmentTitles[i]}
-            </h6>
           </Link>
         ))}
       </section>
