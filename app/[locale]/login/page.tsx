9c7176898fd6b19832bc46db0eb94ba390d7ec50
GetPsyched <priyanshu@getpsyched.dev>
2024-03-30 01:15:33 +0530

    fix: no divs

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -6,8 +6,8 @@ export default function Login({
   params: { locale: string };
 }) {
   return (
-    <div className="flex h-screen grow items-center justify-center">
+    <section className="flex h-screen items-center justify-center">
       <LoginPage locale={locale} />
-    </div>
+    </section>
   );
 }
