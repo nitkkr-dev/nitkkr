3b9a0d2bd321c4d04d7b1e3d5792f74d0943ba98
Antriksh Kumar <68548165+heydoyouknowme0@users.noreply.github.com>
2024-03-30 03:06:42 +0530

    feat: hamburger menu for mobile navbar (#108)
    Co-authored-by: GetPsyched <priyanshu@getpsyched.dev>

--- app/[locale]/header.tsx
+++ app/[locale]/header.tsx
@@ -3,11 +3,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { userAgent } from 'next/server';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
-import { IoMenu } from 'react-icons/io5';
 
 import { CtrlLink } from '~/components/link';
 import LocaleSwitcher from '~/components/locale-switcher';
-import { Button } from '~/components/ui';
+import { Button, HamburgerButton } from '~/components/ui';
 import { getTranslations } from '~/i18n/translations';
 import { cn } from '~/lib/utils';
 
@@ -136,11 +135,46 @@ export default async function Header({ locale }: { locale: string }) {
               </Link>
             </Button>
           </li>
-          <li className="lg:hidden">
-            <IoMenu
-              className="rounded-md bg-primary-700 p-1 text-shade-light hover:cursor-pointer"
-              size={40}
-            />
+          <li className="z-30 font-semibold lg:hidden">
+            <nav className="relative flex h-0">
+              <HamburgerButton
+                className="peer sticky z-40 h-10 w-10 rounded bg-primary-900 transition-colors aria-expanded:bg-transparent"
+                data-dropdownignore={true}
+              />
+              <aside
+                className={cn(
+                  'absolute -right-2 -top-2',
+                  'hidden peer-aria-expanded:flex',
+                  'border border-primary-500 bg-background',
+                  'w-72 max-w-[calc(100vw-1rem)] flex-col gap-y-4 rounded-md p-6'
+                )}
+                data-dropdownignore={true}
+              >
+                <ul
+                  className="space-y-4 text-base font-semibold"
+                  data-dropdownignore
+                >
+                  {items.map(({ label, href }, index) => (
+                    <li key={index} className="w-fit">
+                      <Button
+                        asChild
+                        className="text-left text-shade-dark"
+                        variant="link"
+                      >
+                        <Link href={`/${locale}/${href}`}>{label}</Link>
+                      </Button>
+                    </li>
+                  ))}
+                </ul>
+                <hr className="opacity-50" data-dropdownignore={true} />
+                <Button
+                  asChild
+                  className="bg-primary-900 py-2 text-center sm:rounded"
+                >
+                  <Link href={`/${locale}/login`}>{text.login}</Link>
+                </Button>
+              </aside>
+            </nav>
           </li>
         </ol>
       </nav>
---
+++ components/ui/hamburger-button.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useEventCallback, useScrollLock, useToggle } from 'usehooks-ts';
+
+import { Button } from '~/components/ui/button';
+import { cn } from '~/lib/utils';
+
+const HamburgerButton = ({ className }: { className: string }) => {
+  const { lock, unlock } = useScrollLock({
+    autoLock: false,
+    widthReflow: true,
+  });
+  const [dropdownOpen, toggleDropdown, setDropdownState] = useToggle(false);
+
+  useEffect(() => {
+    if (dropdownOpen) {
+      lock();
+      document.addEventListener('click', handler);
+    } else {
+      unlock();
+      document.removeEventListener('click', handler);
+    }
+    return () => {
+      unlock();
+      document.removeEventListener('click', handler);
+    };
+  }, [dropdownOpen]);
+
+  const handler = useEventCallback((event: MouseEvent) => {
+    if ((event.target as HTMLElement)?.dataset.dropdownignore) return;
+    setDropdownState(false);
+  });
+
+  return (
+    <Button
+      aria-controls="primary-navigation"
+      aria-expanded={dropdownOpen}
+      className={cn(
+        'hamburger-button fill-shade-light aria-expanded:fill-primary-700',
+        'p-1',
+        className
+      )}
+      onClick={toggleDropdown}
+    >
+      <svg viewBox="0 0 100 100">
+        <rect
+          className="line top"
+          height="10"
+          rx="5"
+          width="80"
+          x="10"
+          y="25"
+        />
+        <rect
+          className="line middle"
+          height="10"
+          rx="5"
+          width="80"
+          x="10"
+          y="45"
+        />
+        <rect
+          className="line bottom"
+          height="10"
+          rx="5"
+          width="80"
+          x="10"
+          y="65"
+        />
+      </svg>
+    </Button>
+  );
+};
+
+export { HamburgerButton };
