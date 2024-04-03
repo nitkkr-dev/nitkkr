1f44ac28afb5f974997b28be2f3e454c4219cded
GetPsyched <priyanshu@getpsyched.dev>
2024-04-03 16:31:55 +0530

    fix: add missing employee id

--- server/schema/faculty.schema.ts
+++ server/schema/faculty.schema.ts
@@ -5,6 +5,7 @@ import {
   pgTable,
   smallint,
   text,
+  uniqueIndex,
   varchar,
 } from 'drizzle-orm/pg-core';
 
@@ -18,78 +19,89 @@ import {
   sponsoredResearchProjects,
 } from '.';
 
-export const faculty = pgTable('faculty', {
-  id: integer('id')
-    .primaryKey()
-    .references(() => persons.id),
-  designation: varchar('designation').notNull(),
-  officeTelephone: varchar('office_telephone', { length: 13 }).notNull(),
-  homeTelephone: varchar('home_telephone', { length: 13 }),
-  departmentId: smallint('department_id')
-    .references(() => departments.id)
-    .notNull(),
-  areasOfInterest: text('areas_of_interest')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  teachingInterests: text('teaching_interests')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  researchInterests: text('research_interests')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  patents: text('patents')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  copyrights: text('copyrights')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  journals: text('journals')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  conferences: text('conferences')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  books: text('books')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  workshops: text('workshops')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  researchSupervision: jsonb('research_supervision').notNull(),
-  expertLectures: text('expert_lectures')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  awards: text('awards')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  outreach: text('outreach')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  eContent: text('e_content')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  researchProjects: text('research_projects')
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  googleScholarId: text('google_scholar_id'),
-  orchidId: text('orchid_id'),
-  researcherId: text('researcher_id'),
-  scopusId: text('scopus_id'),
-});
+export const faculty = pgTable(
+  'faculty',
+  {
+    id: integer('id')
+      .primaryKey()
+      .references(() => persons.id),
+    employee_id: varchar('employee_id', { length: 8 }).notNull(),
+    designation: varchar('designation').notNull(),
+    officeTelephone: varchar('office_telephone', { length: 13 }).notNull(),
+    homeTelephone: varchar('home_telephone', { length: 13 }),
+    departmentId: smallint('department_id')
+      .references(() => departments.id)
+      .notNull(),
+    areasOfInterest: text('areas_of_interest')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    teachingInterests: text('teaching_interests')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    researchInterests: text('research_interests')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    patents: text('patents')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    copyrights: text('copyrights')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    journals: text('journals')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    conferences: text('conferences')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    books: text('books')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    workshops: text('workshops')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    researchSupervision: jsonb('research_supervision').notNull(),
+    expertLectures: text('expert_lectures')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    awards: text('awards')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    outreach: text('outreach')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    eContent: text('e_content')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    researchProjects: text('research_projects')
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    googleScholarId: text('google_scholar_id'),
+    orchidId: text('orchid_id'),
+    researcherId: text('researcher_id'),
+    scopusId: text('scopus_id'),
+  },
+  (faculty) => {
+    return {
+      facultyEmployeeIdIndex: uniqueIndex('faculty_employee_id_idx').on(
+        faculty.employee_id
+      ),
+    };
+  }
+);
 
 export const facultyRelations = relations(faculty, ({ many, one }) => ({
   courseLogs: many(courseLogs),
--- server/schema/non-teaching-staff.schema.ts
+++ server/schema/non-teaching-staff.schema.ts
@@ -1,24 +1,41 @@
 import { relations, sql } from 'drizzle-orm';
-import { integer, pgTable, smallint, varchar } from 'drizzle-orm/pg-core';
+import {
+  integer,
+  pgTable,
+  smallint,
+  uniqueIndex,
+  varchar,
+} from 'drizzle-orm/pg-core';
 
 import { departments, persons, sections } from '.';
 
-export const nonTeachingStaff = pgTable('non_teaching_staff', {
-  id: integer('id')
-    .primaryKey()
-    .references(() => persons.id),
-  telephone: varchar('telephone', { length: 13 })
-    .array()
-    .default(sql`'{}'`)
-    .notNull(),
-  workingSectionId: smallint('working_section_id')
-    .references(() => sections.id)
-    .notNull(),
-  designation: varchar('designation').notNull(),
-  workingDepartmentId: smallint('working_department_id')
-    .references(() => departments.id)
-    .notNull(),
-});
+export const nonTeachingStaff = pgTable(
+  'non_teaching_staff',
+  {
+    id: integer('id')
+      .primaryKey()
+      .references(() => persons.id),
+    employee_id: varchar('employee_id', { length: 8 }).notNull(),
+    telephone: varchar('telephone', { length: 13 })
+      .array()
+      .default(sql`'{}'`)
+      .notNull(),
+    workingSectionId: smallint('working_section_id')
+      .references(() => sections.id)
+      .notNull(),
+    designation: varchar('designation').notNull(),
+    workingDepartmentId: smallint('working_department_id')
+      .references(() => departments.id)
+      .notNull(),
+  },
+  (nonTeachingStaff) => {
+    return {
+      nonTeachingStaffEmployeeIdIndex: uniqueIndex(
+        'non_teaching_staff_employee_id_idx'
+      ).on(nonTeachingStaff.employee_id),
+    };
+  }
+);
 
 export const nonTeachingStaffRelations = relations(
   nonTeachingStaff,
