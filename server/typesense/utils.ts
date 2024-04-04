cb06331f78847d49ab4d420d6c8a0ba6ad48ea50
GetPsyched <priyanshu@getpsyched.dev>
2024-04-04 23:35:29 +0530

    feat: API route to fetch query results

---
+++ server/typesense/utils.ts
@@ -0,0 +1,18 @@
+import { client } from '~/server/typesense';
+
+const query_fields = [
+  { name: 'clubs', fields: ['name', 'tagline'] },
+  { name: 'courses', fields: ['code', 'title'] },
+  { name: 'faculty_and_staff', fields: ['name', 'email', 'phone'] },
+];
+
+export async function search(query: string) {
+  return await Promise.all(
+    query_fields.map(({ name, fields }) => {
+      return client
+        .collections(name)
+        .documents()
+        .search({ q: query, query_by: fields.join(',') });
+    })
+  );
+}
