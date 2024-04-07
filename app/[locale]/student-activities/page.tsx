c51fe255067a75808f9546fe915c30af70acf7f3
Antriksh Kumar <68548165+heydoyouknowme0@users.noreply.github.com>
2024-04-08 02:21:57 +0530

    feat: hack to disable header sticky-ness (#120)

--- app/[locale]/student-activities/page.tsx
+++ app/[locale]/student-activities/page.tsx
@@ -21,36 +21,52 @@ export default async function StudentActivities({
 
   return (
     <>
+      <style>
+        {`@media (min-width: 640px) {
+          .header-sticky-ness {
+            position: absolute;
+          }
+        }`}
+      </style>
+
       <article
         className={cn(
-          'relative flex flex-col bg-studentActivities bg-cover bg-top bg-no-repeat',
+          'absolute flex w-full flex-col bg-studentActivities bg-cover bg-top bg-no-repeat max-sm:static',
           'h-56 sm:h-64 md:h-72 lg:h-80 xl:h-96'
         )}
       >
         <h2 className="container my-auto w-fit text-shade-light">
           {text.title}
         </h2>
-        <ol className="absolute bottom-5 hidden w-fit gap-4 self-center rounded-full bg-background p-1 lg:flex xl:p-2">
-          {[
-            { label: text.headings.clubs, href: '#clubs' },
-            { label: text.headings.council, href: '#council' },
-            { label: text.headings.events, href: '#events' },
-            { label: text.headings.thoughtLab, href: '#thought-lab' },
-            { label: text.headings.nss, href: '#nss' },
-            { label: text.headings.ncc, href: '#ncc' },
-          ].map(({ label, href }, index) => (
-            <li key={index}>
-              <Button
-                asChild
-                className="rounded-full px-4 py-2 text-shade-dark transition-colors duration-300"
-                variant="ghost"
-              >
-                <Link href={href}>{label}</Link>
-              </Button>
-            </li>
-          ))}
-        </ol>
       </article>
+
+      <ol
+        className={cn(
+          'p-1 xl:p-2',
+          'hidden sm:flex sm:gap-4',
+          'sticky top-6 mx-auto w-fit self-center rounded-full bg-background',
+          'sm:mt-44 md:mt-52 lg:mt-60 xl:mt-[18.5rem]'
+        )}
+      >
+        {[
+          { label: text.headings.clubs, href: '#clubs' },
+          { label: text.headings.council, href: '#council' },
+          { label: text.headings.events, href: '#events' },
+          { label: text.headings.thoughtLab, href: '#thought-lab' },
+          { label: text.headings.nss, href: '#nss' },
+          { label: text.headings.ncc, href: '#ncc' },
+        ].map(({ label, href }, index) => (
+          <li key={index}>
+            <Button
+              asChild
+              className="rounded-full px-4 py-2 text-shade-dark transition-colors duration-300"
+              variant="ghost"
+            >
+              <Link href={href}>{label}</Link>
+            </Button>
+          </li>
+        ))}
+      </ol>
 
       <Heading
         className="container"
