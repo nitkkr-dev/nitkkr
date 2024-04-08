1201e774d10e7a7728ed5c4d693fd117a8cb55fc
GetPsyched <priyanshu@getpsyched.dev>
2024-04-08 17:34:36 +0530

    fix: carousel arrow colour and responsive styling

--- components/ui/naive-carousel.tsx
+++ components/ui/naive-carousel.tsx
@@ -241,7 +241,7 @@ const CarouselPrevious = React.forwardRef<
       onClick={scrollPrev}
       {...props}
     >
-      <FaChevronLeft className="text-neutral-100" size={40} />
+      <FaChevronLeft className="size-6 sm:size-8 md:size-10" />
     </Button>
   );
 });
@@ -268,7 +268,7 @@ const CarouselNext = React.forwardRef<
       onClick={scrollNext}
       {...props}
     >
-      <FaChevronRight className="text-neutral-100" size={40} />
+      <FaChevronRight className="size-6 sm:size-8 md:size-10" />
     </Button>
   );
 });
