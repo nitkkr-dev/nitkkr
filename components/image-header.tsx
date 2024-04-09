1abf4e2f4e558f86e48e4debbc8b57cac4d70ec7
GetPsyched <priyanshu@getpsyched.dev>
2024-04-09 15:48:05 +0530

    refactor: abstract out image heading

---
+++ components/image-header.tsx
@@ -0,0 +1,67 @@
+import Link from 'next/link';
+
+import { Button } from '~/components/ui';
+import { cn } from '~/lib/utils';
+
+export default function ImageHeader({
+  className,
+  title,
+  headings,
+}: {
+  className?: string;
+  title: string;
+  headings?: { label: string; href: string }[];
+}) {
+  return (
+    <>
+      {headings && (
+        <style>
+          {`@media (min-width: 640px) {
+            .header-sticky-ness {
+              position: static;
+            }
+          }`}
+        </style>
+      )}
+
+      <section
+        className={cn(
+          'absolute flex w-full bg-cover bg-top max-sm:static',
+          'h-56 sm:h-64 md:h-72 lg:h-80 xl:h-96 2xl:h-[448px]',
+          className
+        )}
+      >
+        <h1 className="container my-auto text-center text-shade-light">
+          {title}
+        </h1>
+      </section>
+
+      {headings && (
+        <>
+          <ol
+            className={cn(
+              'p-1 xl:p-2',
+              'hidden sm:flex sm:gap-1 md:gap-2',
+              'sticky top-6 z-nav mx-auto w-fit rounded-full',
+              'border border-primary-700 bg-background drop-shadow-md',
+              'sm:mb-4 sm:mt-48 md:mb-[14px] md:mt-56 lg:mb-[30px] lg:mt-60 xl:mb-[38px] xl:mt-72 2xl:mt-[352px]' // DEPENDS-ON: header.tsx
+            )}
+          >
+            {headings.map(({ label, href }, index) => (
+              <li key={index}>
+                <Button
+                  asChild
+                  className="rounded-full px-4 py-2 text-shade-dark transition-colors duration-300"
+                  variant="ghost"
+                >
+                  <Link href={href}>{label}</Link>
+                </Button>
+              </li>
+            ))}
+          </ol>
+          <hr className="invisible hidden sm:block" />
+        </>
+      )}
+    </>
+  );
+}
