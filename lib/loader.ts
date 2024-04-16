5b7406761fc670a326f82be3bdff6a8b77dfabb5
GetPsyched <priyanshu@getpsyched.dev>
2024-04-16 19:17:34 +0530

    fix: temporarily fix full image urls

--- lib/loader.ts
+++ lib/loader.ts
@@ -7,5 +7,7 @@ export default function imageLoader({
   src: string;
   width: number;
 }) {
-  return `https://isac-nitkkr-public.s3.ap-southeast-2.amazonaws.com/assets/${src}`;
+  return src.startsWith('http')
+    ? src
+    : `https://isac-nitkkr-public.s3.ap-southeast-2.amazonaws.com/assets/${src}`;
 }
