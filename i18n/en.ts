d073cc80a2211b9e3eba1febcfae0d1d0ed25dbb
GetPsyched <priyanshu@getpsyched.dev>
2024-04-16 18:28:28 +0530

    refactor: move director's corner into page

--- i18n/en.ts
+++ i18n/en.ts
@@ -1,6 +1,26 @@
 import type { Translations } from './translations';
 
 const text: Translations = {
+  Main: {
+    director: {
+      alt: 'Prof. B. V. Ramana Reddy',
+      title: 'DIRECTOR’S CORNER',
+      name: 'Prof. B. V. Ramana Reddy',
+      quote: [
+        `India, the land of seekers, is at the cusp of becoming Vishwa Guru all
+      over again after 1100 years of subjugation, wars, annexures and
+      humiliation. It is again a free country due to the sacrifices made by our
+      leaders, freedom fighters and has learnt the art of standing tall in the
+      midst of many a challenge of building the nation with its rich diversity,
+      cultures, languages all over again since the last 75 years. Unity in
+      Diversity is our mantra while making our nation stronger in every
+      sphere.`,
+        'I heartily welcome everyone who visits the website of this institution.',
+      ],
+      more: 'Read more',
+    },
+  },
+
   Committee: {
     building: 'BUILDING & WORK COMMITTEE',
     financial: 'FINANCIAL COMMITTEE',
@@ -29,23 +49,6 @@ const text: Translations = {
       programmes: 'Programmes',
       gallery: 'Gallery',
     },
-  },
-  DirectorsCorner: {
-    alt: 'Prof. B. V. Ramana Reddy',
-    title: 'DIRECTOR’S CORNER',
-    name: 'Prof. B. V. Ramana Reddy',
-    quote: [
-      `India, the land of seekers, is at the cusp of becoming Vishwa Guru all
-      over again after 1100 years of subjugation, wars, annexures and
-      humiliation. It is again a free country due to the sacrifices made by our
-      leaders, freedom fighters and has learnt the art of standing tall in the
-      midst of many a challenge of building the nation with its rich diversity,
-      cultures, languages all over again since the last 75 years. Unity in
-      Diversity is our mantra while making our nation stronger in every
-      sphere.`,
-      'I heartily welcome everyone who visits the website of this institution.',
-    ],
-    more: 'Read more',
   },
   Footer: {
     logo: 'Logo',
