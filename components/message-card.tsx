ed24f53c27a5ada7462f06ace119675659da44fd
Antriksh Kumar <68548165+heydoyouknowme0@users.noreply.github.com>
2024-04-17 12:08:31 +0530

    style: better layout styling for message card  (#136)

--- components/message-card.tsx
+++ components/message-card.tsx
@@ -34,26 +34,25 @@ export default function MessageCard({
   return (
     <article
       className={cn(
-        'grid gap-5',
-        'grid-cols-[5rem,auto] md:grid-cols-[5rem,auto] lg:grid-cols-[min(30%,24rem),auto] lg:grid-rows-[4rem,auto]',
+        'grid gap-x-5 space-y-3 sm:space-y-4 md:space-y-5',
+        'grid-cols-[5rem,auto] md:grid-cols-[5rem,auto] lg:grid-cols-[min(30%,24rem),auto] lg:grid-rows-[3rem,auto]',
         'p-4 md:p-6 xl:p-8',
         'rounded-xl border border-primary-700 bg-neutral-50',
         className
       )}
-      style={{ gridAutoRows: 'min-content' }}
     >
       <Image
         alt={name}
-        className="col-span-1 aspect-[6/7] rounded-xl object-cover max-lg:w-24 lg:row-span-2"
+        className="aspect-[6/7] rounded-xl object-cover max-lg:w-24 lg:row-span-2"
         height={500}
         src={image}
         width={500}
       />
-      <h3 className="mb-0 h-fit self-center lg:text-4xl">{name}</h3>
+      <h3 className="!my-0 h-fit self-center lg:text-4xl">{name}</h3>
       <p
         className={cn(
           'max-lg:col-span-2 lg:text-xl',
-          details && 'lg:row-span-2'
+          details && 'lg:row-span-3'
         )}
       >
         {quote}
@@ -89,15 +88,15 @@ export default function MessageCard({
         <footer className="max-lg:col-span-2">
           <ul>
             <li className="flex items-center gap-2">
-              <MdEmail className="fill-primary-700" />
+              <MdEmail className="size-4 fill-primary-700 md:size-5" />
               {details.email}
             </li>
             <li className="flex items-center gap-2">
-              <FaPhone className="fill-primary-700" />
+              <FaPhone className="size-4 fill-primary-700 md:size-5" />
               {details.phone}
             </li>
             <li className="flex items-center gap-2">
-              <FaCalendar className="fill-primary-700" />
+              <FaCalendar className="size-4 fill-primary-700 md:size-5" />
               {details.session}
             </li>
           </ul>
