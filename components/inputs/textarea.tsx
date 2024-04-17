fee33118e511561a2ff02ef10ecd81a460bcdb71
antri <akrb2204@gmail.com>
2024-04-17 12:00:23 +0530

    style: add custom styling

--- components/inputs/textarea.tsx
+++ components/inputs/textarea.tsx
@@ -2,15 +2,20 @@ import * as React from 'react';
 
 import { cn } from '~/lib/utils';
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
         className={cn(
-          'border-gray-200 bg-white ring-offset-white placeholder:text-gray-500 focus-visible:ring-gray-950 dark:border-gray-800 dark:bg-gray-950 dark:ring-offset-gray-950 dark:placeholder:text-gray-400 dark:focus-visible:ring-gray-300 flex min-h-[80px] w-full rounded-md border px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex min-h-[60px]',
+          'w-full bg-neutral-50 px-3 py-2',
+          'rounded-md border shadow-sm',
+          'text-xs sm:text-sm',
+          'placeholder:text-neutral-500 invalid:border-primary-500 invalid:outline-offset-2 invalid:outline-primary-100',
+          'focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-800',
+          'disabled:cursor-not-allowed disabled:border-0 disabled:bg-neutral-200/30 disabled:ring-0',
           className
         )}
         ref={ref}
