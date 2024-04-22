4fb652c8b37e0916762c394403f48dec6540909f
GetPsyched <priyanshu@getpsyched.dev>
2024-04-23 02:45:52 +0530

    feat: clubs tab

--- app/[locale]/profile/clubs/page.tsx
+++ app/[locale]/profile/clubs/page.tsx
@@ -1,5 +1,9 @@
-import WorkInProgress from '~/components/work-in-progress';
+import Image from 'next/image';
+
 import { getTranslations } from '~/i18n/translations';
+import { capitalise } from '~/lib/utils';
+import { getServerAuthSession } from '~/server/auth';
+import { db } from '~/server/db';
 
 export default async function Clubs({
   params: { locale },
@@ -8,6 +12,22 @@ export default async function Clubs({
 }) {
   const text = (await getTranslations(locale)).Profile.tabs.clubs;
 
+  const session = (await getServerAuthSession())!;
+  const student = (await db.query.students.findFirst({
+    columns: {},
+    where: (student, { eq }) => eq(student.id, session.person.id),
+    with: {
+      clubMembers: {
+        columns: { position: true },
+        with: {
+          club: {
+            columns: { alias: true, logo: true, name: true, tagline: true },
+          },
+        },
+      },
+    },
+  }))!;
+
   return (
     <>
       <header className="mb-4 max-md:hidden">
@@ -15,7 +35,26 @@ export default async function Clubs({
         <hr className="border border-primary-700 opacity-50" />
       </header>
 
-      <WorkInProgress locale={locale} />
+      <ul className="space-y-4">
+        {student.clubMembers.map(
+          ({ club: { alias, logo, name, tagline }, position }, index) => (
+            <li className="flex gap-4" key={index}>
+              <Image
+                alt={alias ?? name}
+                className="h-16 min-w-16 rounded-md"
+                height={0}
+                src={logo}
+                width={0}
+              />
+              <section>
+                <p className="font-bold text-primary-700">{name}</p>
+                <span className="font-medium">{tagline}</span>
+                <p className="text-neutral-600">{capitalise(position)}</p>
+              </section>
+            </li>
+          )
+        )}
+      </ul>
     </>
   );
 }
