5a7131385f8f9b7c753896cccb1b859ac253a50e
GetPsyched <priyanshu@getpsyched.dev>
2024-04-25 16:41:38 +0530

    feat: allow custom class names

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,5 +1,11 @@
 import { AiOutlineLoading } from 'react-icons/ai';
 
-export default function Loading() {
-  return <AiOutlineLoading className="m-auto h-5 w-5 animate-spin" />;
+import { cn } from '~/lib/utils';
+
+export default function Loading({ className }: { className?: string }) {
+  return (
+    <AiOutlineLoading
+      className={cn('m-auto h-5 w-5 animate-spin', className)}
+    />
+  );
 }
