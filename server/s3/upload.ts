22b6708de96d2955b0d86893c001641c071fe9e5
Priyanshu Tripathi <priyanshu@getpsyched.dev>
2024-04-26 06:05:36 +0530

    feat: uploadFileToS3 method (#165)
    Co-authored-by: SaketRaman2002 <raman.saket17@gmail.com>

---
+++ server/s3/upload.ts
@@ -0,0 +1,16 @@
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+
+import { env } from '~/lib/env';
+
+import { s3 } from '.';
+
+export async function uploadFileToS3(file: File, filePath: string) {
+  return await s3.send(
+    new PutObjectCommand({
+      Body: Buffer.from(await file.arrayBuffer()),
+      Bucket: env.AWS_PUBLIC_S3_NAME,
+      ContentType: file.type,
+      Key: filePath,
+    })
+  );
+}
