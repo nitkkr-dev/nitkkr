13c412413dd7fa8e720be7c45de722a6ad5511f4
antri <akrb2204@gmail.com>
2024-04-27 12:15:06 +0530

    feat: moved search and filters to client, with native navigation to stop refresh

---
+++ app/[locale]/faculty-and-staff/client-utils.tsx
@@ -0,0 +1,177 @@
+'use client';
+
+import { type InferSelectModel } from 'drizzle-orm';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import Image from 'next/image';
+import { MdEmail } from 'react-icons/md';
+import { FaPhone } from 'react-icons/fa';
+import { useDebounceCallback } from 'usehooks-ts';
+
+import {
+  Input,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/inputs';
+import { Button } from '~/components/ui';
+import { type departments as departmentsTable } from '~/server/db';
+
+export const Tabs = ({
+  select = false,
+  departments,
+}: {
+  select?: boolean;
+  departments: Pick<
+    InferSelectModel<typeof departmentsTable>,
+    'id' | 'name' | 'urlName'
+  >[];
+}) => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get('query') ?? '';
+  const currentDepartmentUrl = searchParams.get('department') ?? undefined;
+
+  return select ? (
+    <Select
+      defaultValue={
+        currentDepartmentUrl && `department=${currentDepartmentUrl}`
+      }
+      onValueChange={(value) =>
+        window.history.replaceState(null, '', `?query=${query}&${value}`)
+      }
+    >
+      <SelectTrigger className="px-4 py-5 sm:w-1/2 lg:w-1/3 xl:hidden">
+        <SelectValue placeholder="Choose a department" />
+      </SelectTrigger>
+      <SelectContent>
+        {departments.map(({ name, urlName }, index) => (
+          <SelectItem key={index} value={`department=${urlName}`}>
+            {name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  ) : (
+    <ol className="w-full space-y-4">
+      {departments.map(({ name, urlName }, index) => (
+        <li key={index}>
+          <Button
+            active={urlName === currentDepartmentUrl}
+            className="font-semibold text-shade-dark"
+            variant={'link'}
+            onClick={() =>
+              window.history.replaceState(null, '', `?department=${urlName}`)
+            }
+          >
+            {name}
+          </Button>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export const FilteredFacultyList = ({
+  departments,
+  departmentHeads,
+  deptartmentHeadText,
+  faculty,
+  locale,
+}: {
+  departments: Map<string, number>;
+  departmentHeads: Set<number>;
+  deptartmentHeadText: string;
+  faculty: {
+    employeeId: string;
+    id: number;
+    person: {
+      email: string;
+      name: string;
+      telephone: string;
+    };
+    departmentId: number;
+    designation:
+      | 'Assistant Professor Grade-I'
+      | 'Assistant Professor Grade-II'
+      | 'Associate Professor'
+      | 'Professor';
+  }[];
+  locale: string;
+}) => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get('query') ?? '';
+  const currentDepartmentId = departments.get(
+    searchParams.get('department') ?? ''
+  );
+  return faculty
+    .filter(
+      ({ person, departmentId }) =>
+        person.name.toLowerCase().includes(query.toLowerCase()) &&
+        (!currentDepartmentId || departmentId === currentDepartmentId)
+    )
+    .map((faculty, index) => (
+      <li
+        className="rounded border border-primary-700 bg-neutral-50 hover:drop-shadow-md"
+        key={index}
+      >
+        <Link
+          className="flex gap-4 p-2 sm:p-3 md:p-4"
+          href={`/${locale}/faculty-and-staff/${faculty.employeeId}`}
+        >
+          <Image
+            alt={faculty.person.name}
+            className="size-32 rounded lg:size-36 xl:size-40 2xl:size-44"
+            height={0}
+            src={`persons/${faculty.id}/image.png`}
+            width={0}
+          />
+          <main>
+            <header className="mb-1 sm:mb-2 md:mb-3 lg:mb-4">
+              <h4 className="mb-0">{faculty.person.name}</h4>
+              <p>
+                {faculty.designation}
+                {departmentHeads.has(faculty.id) && ` (${deptartmentHeadText})`}
+              </p>
+            </header>
+
+            <ul>
+              <li className="flex items-center gap-2">
+                <MdEmail className="fill-primary-700" />
+                {faculty.person.email}
+              </li>
+              <li className="flex items-center gap-2">
+                <FaPhone className="fill-primary-700" />
+                {faculty.person.telephone}
+              </li>
+            </ul>
+          </main>
+        </Link>
+      </li>
+    ));
+};
+
+export const SearchInput = ({
+  defaultValue,
+  placeholder,
+}: {
+  defaultValue?: string;
+  placeholder: string;
+}) => {
+  const debounceCallback = useDebounceCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      window.history.replaceState(null, '', `?query=${event.target.value}`);
+    },
+    100
+  );
+  return (
+    <Input
+      className="sm:grow"
+      defaultValue={defaultValue}
+      id="faculty-or-staff"
+      onChange={debounceCallback}
+      placeholder={placeholder}
+    />
+  );
+};
--- app/[locale]/faculty-and-staff/page.tsx
+++ app/[locale]/faculty-and-staff/page.tsx
@@ -1,22 +1,11 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import { Suspense } from 'react';
-import { FaPhone } from 'react-icons/fa6';
-import { MdEmail } from 'react-icons/md';
 
-import {
-  Input,
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '~/components/inputs';
 import Loading from '~/components/loading';
-import { Button } from '~/components/ui';
 import { getTranslations } from '~/i18n/translations';
 import { cn } from '~/lib/utils';
 import { db } from '~/server/db';
+
+import { FilteredFacultyList, SearchInput, Tabs } from './client-utils';
 
 export default async function FacultyAndStaff({
   params: { locale },
@@ -37,33 +26,24 @@ export default async function FacultyAndStaff({
         )}
       >
         <Suspense fallback={<Loading className="max-xl:hidden" />}>
-          <Departments department={departmentName} />
+          <Departments />
         </Suspense>
       </search>
 
       <section className="grow space-y-6">
         <search className="flex gap-4 max-sm:flex-col">
-          <Input
-            className="sm:grow"
-            debounceTo="query"
-            debounceEvery={100}
-            defaultValue={query}
-            id="faculty-or-staff"
-            placeholder={text.placeholder}
-          />
+          <SearchInput defaultValue={query} placeholder={text.placeholder} />
 
           <Suspense fallback={<Loading className="xl:hidden" />}>
-            <Departments department={departmentName} select />
+            <Departments select />
           </Suspense>
         </search>
 
         <ol className="space-y-4">
           <Suspense fallback={<Loading />} key={`${query}-${departmentName}`}>
             <FacultyList
-              department={departmentName}
               deptartmentHeadText={text.departmentHead}
               locale={locale}
-              query={query}
             />
           </Suspense>
         </ol>
@@ -72,134 +52,71 @@ export default async function FacultyAndStaff({
   );
 }
 
-const Departments = async ({
-  department,
-  select = false,
-}: {
-  department?: string;
-  select?: boolean;
-}) => {
+const Departments = async ({ select = false }: { select?: boolean }) => {
   const departments = await db.query.departments.findMany({
     columns: { id: true, name: true, urlName: true },
   });
-  const currentDepartment = departments.find(
-    ({ urlName }) => urlName === department
-  );
 
-  return select ? (
-    <Select
-      defaultValue={currentDepartment && `?department=${department}`}
-      navigate
-    >
-      <SelectTrigger className="px-4 py-5 sm:w-1/2 lg:w-1/3 xl:hidden">
-        <SelectValue placeholder="Choose a department" />
-      </SelectTrigger>
-      <SelectContent>
-        {departments.map(({ name, urlName }, index) => (
-          <SelectItem key={index} value={`?department=${urlName}`}>
-            {name}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  ) : (
-    <ol className="w-full space-y-4">
-      {departments.map(({ name, urlName }, index) => (
-        <li key={index}>
-          <Button
-            active={urlName === currentDepartment?.urlName}
-            asChild
-            className="font-semibold text-shade-dark"
-            variant="link"
-          >
-            <Link href={{ query: { department: urlName } }}>{name}</Link>
-          </Button>
-        </li>
-      ))}
-    </ol>
-  );
+  return <Tabs select={select} departments={departments} />;
 };
 
 const FacultyList = async ({
-  department,
   deptartmentHeadText,
   locale,
-  query,
 }: {
-  department?: string;
   deptartmentHeadText: string;
   locale: string;
-  query?: string;
 }) => {
-  const departments = await db.query.departments.findMany({
-    columns: { id: true, name: true, urlName: true },
-  });
-  const departmentHeads = await db.query.departmentHeads.findMany({
-    where: (departmentHead, { eq }) => eq(departmentHead.isActive, true),
-  });
-  const currentDepartment = departments.find(
-    ({ urlName }) => urlName === department
-  );
+  const departments = new Map<string, number>();
+  const departmentHeads = new Set<number>();
+  await db.query.departments
+    .findMany({
+      columns: { id: true, urlName: true },
+    })
+    .then(
+      async (departmentsArray) =>
+        await Promise.all(
+          departmentsArray.map(async (department) => {
+            departments.set(department.urlName, department.id);
+          })
+        )
+    );
+
+  await db.query.departmentHeads
+    .findMany({
+      where: (departmentHead, { eq }) => eq(departmentHead.isActive, true),
+      columns: { facultyId: true },
+    })
+    .then(
+      async (departmentHeadsArray) =>
+        await Promise.all(
+          departmentHeadsArray.map(async (departmentHead) => {
+            departmentHeads.add(departmentHead.facultyId);
+          })
+        )
+    );
+
+  //const currentDepartmentId = departments.get(department ?? '');
 
   const faculty = await db.query.faculty.findMany({
     columns: {
       designation: true,
       employeeId: true,
       id: true,
+      departmentId: true,
     },
-    where: currentDepartment
-      ? (faculty, { eq }) => eq(faculty.departmentId, currentDepartment.id)
-      : undefined,
+    // where: currentDepartment
+    //   ? (faculty, { eq }) => eq(faculty.departmentId, currentDepartment.id)
+    //   : undefined,
     with: { person: { columns: { email: true, name: true, telephone: true } } },
   });
-
-  return faculty
-    .filter(({ person }) =>
-      person.name.toLowerCase().includes((query ?? '').toLowerCase())
-    )
-    .map((faculty, index) => {
-      const isDepartmentHead = departmentHeads.find(
-        ({ facultyId }) => facultyId === faculty.id
-      );
-
-      return (
-        <li
-          className="rounded border border-primary-700 bg-neutral-50 hover:drop-shadow-md"
-          key={index}
-        >
-          <Link
-            className="flex gap-4 p-2 sm:p-3 md:p-4"
-            href={`/${locale}/faculty-and-staff/${faculty.employeeId}`}
-          >
-            <Image
-              alt={faculty.person.name}
-              className="size-32 rounded lg:size-36 xl:size-40 2xl:size-44"
-              height={0}
-              src={`persons/${faculty.id}/image.png`}
-              width={0}
-            />
-            <main>
-              <header className="mb-1 sm:mb-2 md:mb-3 lg:mb-4">
-                <h4 className="mb-0">{faculty.person.name}</h4>
-                <p>
-                  {faculty.designation}
-                  {isDepartmentHead && ` (${deptartmentHeadText})`}
-                </p>
-              </header>
-
-              <ul>
-                <li className="flex items-center gap-2">
-                  <MdEmail className="fill-primary-700" />
-                  {faculty.person.email}
-                </li>
-                <li className="flex items-center gap-2">
-                  <FaPhone className="fill-primary-700" />
-                  {faculty.person.telephone}
-                </li>
-              </ul>
-            </main>
-          </Link>
-        </li>
-      );
-    });
+  return (
+    <FilteredFacultyList
+      departments={departments}
+      departmentHeads={departmentHeads}
+      deptartmentHeadText={deptartmentHeadText}
+      faculty={faculty}
+      locale={locale}
+    />
+  );
 };
