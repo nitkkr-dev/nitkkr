f8b70b03f98c08deec50b091956fca9700d8b8cf
geekysilento <adityakarna.ak@gmail.com>
2024-05-13 08:07:08 +0530

    fix: section pages runtime error

--- app/[locale]/institute/sections/sports/page.tsx
+++ app/[locale]/institute/sections/sports/page.tsx
@@ -2,7 +2,11 @@ import WorkInProgress from '~/components/work-in-progress';
 import { getTranslations } from '~/i18n/translations';
 import { db } from '~/server/db';
 
-export default async function Sports({ locale }: { locale: string }) {
+export default async function Sports({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
   const text = (await getTranslations(locale)).Section.Sports;
 
   const section = (await db.query.sections.findFirst({
