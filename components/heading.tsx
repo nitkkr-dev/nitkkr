4b2d96ef7534b90185b9a57b59ed6dfa0ff4115a
heydoyouknowme0 <akrb2204@gmail.com>
2024-05-13 09:43:23 +0530

    fix: proper hover and hash size

--- components/heading.tsx
+++ components/heading.tsx
@@ -62,7 +62,20 @@ function Horses({ direction }: { direction: 'rtl' | 'ltr' }) {
     </figure>
   );
 }
-
+const heightText = (Comp: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6') => {
+  switch (Comp) {
+    case 'h1':
+      return 'text-3xl sm:text-4xl md:text-5xl';
+    case 'h2':
+      return 'text-2xl sm:text-3xl md:text-4xl';
+    case 'h3':
+      return 'text-xl sm:text-2xl md:text-3xl';
+    case 'h4':
+      return 'text-lg sm:text-xl md:text-2xl';
+    case 'h5':
+      return 'text-base sm:text-lg md:text-xl';
+  }
+};
 export default function Heading({
   className,
   glyphDirection,
@@ -80,30 +93,42 @@ export default function Heading({
         'group relative',
         'my-4 sm:my-6 md:my-8 lg:my-10 xl:my-12',
         'flex flex-nowrap items-center space-x-2 sm:space-x-3 md:space-x-4',
+        glyphDirection === 'rtl' && 'flex-row-reverse',
         className
       )}
       {...props}
     >
-      {glyphDirection === 'dual' && <Elephants direction="rtl" />}
-      {glyphDirection === 'rtl' && <Horses direction={glyphDirection} />}
-      <Comp className="my-auto min-w-fit">{text}</Comp>
-      {glyphDirection === 'ltr' && <Horses direction={glyphDirection} />}
-      {glyphDirection === 'dual' && <Elephants direction="ltr" />}
-
-      {glyphDirection !== 'dual' && (
-        <Button
-          asChild
+      {glyphDirection === 'dual' ? (
+        <Elephants direction="rtl" />
+      ) : (
+        <span
           className={cn(
-            '!ml-0 w-0',
-            'hidden animate-in fade-in zoom-in group-hover:inline',
-            // FIXME: First/last ordering
-            glyphDirection === 'ltr' && 'order-first after:md:pr-2',
-            glyphDirection === 'rtl' && 'order-last before:md:pl-2'
+            '!ml-0 flex w-0 before:md:pl-2',
+            glyphDirection === 'ltr' && 'flex-row-reverse'
           )}
-          variant="link"
         >
-          <Link href={`#${props.id}`}>#</Link>
-        </Button>
+          <Button
+            asChild
+            className={cn(
+              'hidden animate-in fade-in zoom-in group-hover:inline',
+              heightText(Comp)
+            )}
+            variant="link"
+          >
+            <Link href={`#${props.id}`}>#</Link>
+          </Button>
+        </span>
+      )}
+      <Comp
+        className={cn('my-auto min-w-fit', glyphDirection === 'ltr' && '!ml-0')}
+      >
+        {text}
+      </Comp>
+
+      {glyphDirection === 'dual' ? (
+        <Elephants direction="ltr" />
+      ) : (
+        <Horses direction={glyphDirection} />
       )}
     </header>
   );
