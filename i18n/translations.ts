6969a8fd81812b35f554316d0f13c23df912b39a
GetPsyched <priyanshu@getpsyched.dev>
2024-06-26 16:41:08 +0400

    fix: translation schema and coordinator typo

--- i18n/translations.ts
+++ i18n/translations.ts
@@ -47,21 +47,20 @@ export interface Translations {
     credits: string;
     totalCredits: string;
     syllabus: string;
-    Details: {
-      courseCode: string;
-      title: string;
-      coordinator: string;
-      prerequisites: string;
-      nature: string;
-      objectives: string;
-      content: string;
-      outcomes: string;
-      essentialReading: string;
-      supplementaryReading: string;
-      similarCourses: string;
-      headOfDepartment: string;
-      referenceBooks: string;
-    };
+  };
+  Curriculum: {
+    courseCode: string;
+    title: string;
+    coordinator: string;
+    prerequisites: string;
+    nature: string;
+    objectives: string;
+    content: string;
+    outcomes: string;
+    essentialReading: string;
+    supplementaryReading: string;
+    similarCourses: string;
+    referenceBooks: string;
   };
   Dean: {
     deanTitles: {
