b153afb81354302a366d009f5f65e4f0461f6476
Aditya Karna <125768789+geekysilento@users.noreply.github.com>
2024-09-02 22:55:14 +0530

    feat: add missing NITKKR title (#218)
    Also add title and descriptions to other slides

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -43,9 +43,17 @@ export default async function Home({
             },
             {
               image: 'slideshow/image02.jpg',
+              title:
+                'National Institute Ranked Among Top 10 Engineering Colleges',
+              subtitle:
+                'NIT Kurukshetra has secured a spot in the top 10 engineering colleges in India, showcasing excellence in education and research...',
             },
             {
               image: 'slideshow/image03.jpg',
+              title:
+                'State-of-the-Art Research Facilities Now Open for Students',
+              subtitle:
+                'The newly inaugurated research labs and centers at NIT KKR offer cutting-edge technology and resources for students and faculty alike...',
             },
           ].map(({ image, title, subtitle }, index) => (
             <CarouselItem key={index} className="relative max-h-screen">
@@ -58,6 +66,15 @@ export default async function Home({
                   width={1920}
                   src={image}
                 />
+                {index === 0 && (
+                  <article className="absolute inset-x-0 top-[30%] mx-auto text-center drop-shadow-md">
+                    <h4 className="text-shade-light">एनआईटी कुरूक्षेत्</h4>
+                    <h1 className="text-6xl text-shade-light">
+                      NIT Kurukshetra
+                    </h1>
+                  </article>
+                )}
+
                 {title && (
                   <figcaption className="absolute inset-x-0 bottom-0 min-w-full bg-gradient-to-b from-transparent to-neutral-800 py-6">
                     <article className="container">
