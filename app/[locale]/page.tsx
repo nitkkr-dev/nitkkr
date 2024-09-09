12cb057fcf0e8830fc52d81d534d965887610d49
Aditya Karna <125768789+geekysilento@users.noreply.github.com>
2024-09-09 18:52:10 +0530

    feat: align slideshow with design (#220)
    - Add NIT title to each image
    - Add contact icons

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
+import { BsLinkedin } from 'react-icons/bs';
+import { MdEmail, MdPhone } from 'react-icons/md';
 
 import Notifications from '~/app/notifications';
+import { Button } from '~/components/buttons';
 import {
   AutoplayCarousel,
   CarouselContent,
@@ -57,7 +60,16 @@ export default async function Home({
             },
           ].map(({ image, title, subtitle }, index) => (
             <CarouselItem key={index} className="relative max-h-screen">
-              <figure className="h-full">
+              <figure className="relative h-full">
+                {/* Gradient Overlay */}
+                <div className="absolute inset-0 bg-gradient-to-l from-transparent to-neutral-800/80"></div>
+                <article className="absolute inset-x-0 top-[35%] mx-auto text-center drop-shadow-md">
+                  <h4 className="text-shade-light">एनआईटी कुरूक्षेत्</h4>
+                  <h1 className="text-lg text-shade-light md:text-4xl lg:text-6xl">
+                    NIT KURUKSHETRA
+                  </h1>
+                </article>
+
                 <Image
                   alt={`slide ${index + 1}`}
                   className="h-full w-full object-cover"
@@ -66,20 +78,16 @@ export default async function Home({
                   width={1920}
                   src={image}
                 />
-                {index === 0 && (
-                  <article className="absolute inset-x-0 top-[30%] mx-auto text-center drop-shadow-md">
-                    <h4 className="text-shade-light">एनआईटी कुरूक्षेत्</h4>
-                    <h1 className="text-6xl text-shade-light">
-                      NIT Kurukshetra
-                    </h1>
-                  </article>
-                )}
 
                 {title && (
-                  <figcaption className="absolute inset-x-0 bottom-0 min-w-full bg-gradient-to-b from-transparent to-neutral-800 py-6">
-                    <article className="container">
-                      <h4 className="pl-2 text-neutral-100">{title}</h4>
-                      <p className="pl-2 text-neutral-100">{subtitle}</p>
+                  <figcaption className="container absolute inset-x-0 bottom-0 justify-start pb-8 lg:block">
+                    <article className="max-w-[90%]">
+                      <h4 className="pl-2 text-sm text-neutral-100 md:text-2xl">
+                        {title}
+                      </h4>
+                      <p className="line-clamp-1 pl-2 text-xs text-neutral-100 md:text-lg">
+                        {subtitle}
+                      </p>
                     </article>
                   </figcaption>
                 )}
@@ -91,6 +99,28 @@ export default async function Home({
         <section className="container absolute inset-0 top-[50%] flex h-0 justify-between">
           <CarouselPrevious className="static text-neutral-100 drop-shadow-2xl" />
           <CarouselNext className="static text-neutral-100 drop-shadow-2xl" />
+        </section>
+
+        <section className="container absolute inset-x-0 bottom-0 hidden h-0 justify-end lg:flex">
+          <section className="absolute bottom-0 flex flex-col space-y-4 pb-8">
+            {[
+              { href: 'mailto:director@nitkkr.ac.in', icon: MdEmail },
+              { href: 'tel:+911742238570', icon: MdPhone },
+              {
+                href: 'https://www.linkedin.com/school/national-institute-of-technology-kurukshetra-haryana',
+                icon: BsLinkedin,
+              },
+            ].map(({ href, icon: Icon }, index) => (
+              <a href={href} key={index}>
+                <Button
+                  className="size-16 rounded-full border border-shade-light text-neutral-900 backdrop-blur-md hover:bg-shade-light/20"
+                  variant="icon"
+                >
+                  <Icon className="size-5 text-shade-light lg:size-8" />
+                </Button>
+              </a>
+            ))}
+          </section>
         </section>
       </AutoplayCarousel>
 
