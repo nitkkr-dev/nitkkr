33a38b60dfc0480e8b61b55949f99198b57f734d
aryansri-19 <aryansri.19@gmail.com>
2025-02-03 20:47:07 +0530

    Added Administration Static Page

--- i18n/translations.ts
+++ i18n/translations.ts
@@ -5,16 +5,38 @@ export async function getTranslations(locale: string): Promise<Translations> {
 export interface Translations {
   Administration: {
     title: string;
-    boardOfDirectors: string;
+    boardOfGovernors: string;
+    bogAgenda: string;
+    bogMinutes: string;
+    constitutionOfBoG: string;
     buildingAndWork: string;
     financial: string;
     senate: string;
-    committees: string;
+    composition: string;
+    sNo: string;
+    name: string;
+    servedAs: string;
+    senateMeetingAgenda: string;
+    senateMeetingMinutes: string;
+    scsaMeetingMinutes: string;
+    administrationHeads: string;
     director: string;
     deans: string;
+    otherOfficers: string;
+    committees: string;
+    actsAndStatutes: string;
+    actsPoints: {
+      text: string;
+      link: string;
+    }[];
     and: string;
     description: string;
-    about: string;
+    approvalHeading: string;
+    approvalDescription: string;
+    pointsOfApproval: {
+      text: string;
+      link: string;
+    }[];
   };
   Awards: {
     aboutTitle: string;
