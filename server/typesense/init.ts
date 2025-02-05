1342a701dfc9a9357610828448f9b994887c9a01
heydoyouknowme0 <68548165+heydoyouknowme0@users.noreply.github.com>
2025-02-06 00:18:38 +0530

    temp

--- server/typesense/init.ts
+++ server/typesense/init.ts
@@ -2,8 +2,9 @@ import { Client } from 'typesense';
 
 import { env } from '~/lib/env/server';
 
-const createTypesenseClient = () =>
-  new Client({
+const createTypesenseClient = () => {
+  console.log('production is', env.NODE_ENV);
+  return new Client({
     nodes: [
       {
         host: env.TYPESENSE_HOST,
@@ -16,7 +17,7 @@ const createTypesenseClient = () =>
     apiKey: env.TYPESENSE_API_KEY,
     connectionTimeoutSeconds: 2,
   });
-
+};
 const globalForTypesense = globalThis as unknown as {
   typesense: ReturnType<typeof createTypesenseClient>;
 };
