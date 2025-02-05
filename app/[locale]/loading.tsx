8ff7181cfc9b09d07f202921bc7bad71fc3a2962
heydoyouknowme0 <68548165+heydoyouknowme0@users.noreply.github.com>
2025-02-05 22:41:06 +0530

    added loading

---
+++ app/[locale]/loading.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Loading from '~/components/loading';
+
+export default function Page() {
+  return (
+    <main className="global container grid h-dvh place-items-center">
+      <Loading />
+    </main>
+  );
+}
