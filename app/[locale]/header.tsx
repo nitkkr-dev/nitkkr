e7df0a143b33470a947031cab77109cde645e37d
heydoyouknowme0 <68548165+heydoyouknowme0@users.noreply.github.com>
2025-03-27 11:25:34 +0000

    fixxx

--- app/[locale]/header.tsx
+++ app/[locale]/header.tsx
@@ -5,6 +5,7 @@ import { userAgent } from 'next/server';
 import { Suspense } from 'react';
 import { BsPersonFill } from 'react-icons/bs';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
+import { MdArrowBackIosNew } from 'react-icons/md';
 
 import {
   Button,
@@ -248,13 +249,14 @@ export default async function Header({ locale }: { locale: string }) {
                   <header className="mb-6 mt-2 h-10 sm:mb-8 sm:mt-4 md:mb-10 md:mt-6">
                     <SwitchNavButton
                       column="default"
-                      text={'〉'}
                       props={{
                         className:
-                          'nav-column-academics  nav-column-institute invisible my-auto !h-full text-xl mx-2 font-bold',
+                          'nav-column-academics nav-column-institute invisible my-auto !h-full text-xl font-bold',
                         variant: 'link',
                       }}
-                    />
+                    >
+                      <MdArrowBackIosNew className="size-8" />
+                    </SwitchNavButton>
                   </header>
                   <ul className="nav-column-default space-y-4 text-base font-semibold">
                     {items.map(({ label, href, listItems }, index) => (
@@ -436,7 +438,7 @@ const MobileSubNavMenu = ({
     >
       <NavButton asChild variant="icon">
         <Link
-          href={'/' + href}
+          href={`/${locale}/${href}`}
           className="group relative mb-6 !flex aspect-[5/2] min-h-20 select-none !flex-col justify-end overflow-hidden rounded-xl no-underline outline-none"
         >
           <Image
