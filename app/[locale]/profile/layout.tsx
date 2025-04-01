e50025802e4ee56bb2d7f4daa568df020866b4eb
heydoyouknowme0 <68548165+heydoyouknowme0@users.noreply.github.com>
2025-04-01 17:43:45 +0000

    fix: faculty profile page

--- app/[locale]/profile/layout.tsx
+++ app/[locale]/profile/layout.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 
 import { UnauthorisedStatus } from '~/components/status';
+import { ScrollArea } from '~/components/ui';
 import { getTranslations } from '~/i18n/translations';
 import { cn } from '~/lib/utils';
 import { getServerAuthSession } from '~/server/auth';
 import { db } from '~/server/db';
-import { ScrollArea } from '~/components/ui';
 
+import { FacultyOrStaffComponent } from '../faculty-and-staff/utils';
 import { LogOut, PathnameAwareSuspense, Tabs } from './client-utils';
 
 export default async function ProfileLayout({
@@ -18,7 +19,12 @@ export default async function ProfileLayout({
 }) {
   const session = await getServerAuthSession();
   if (!session) return <UnauthorisedStatus locale={locale} />;
-
+  else if (session.person.type == 'faculty')
+    return (
+      <FacultyOrStaffComponent id={session.person.id} locale={locale}>
+        {children}
+      </FacultyOrStaffComponent>
+    );
   const text = (await getTranslations(locale)).Profile;
 
   const student = (await db.query.students.findFirst({
--- app/[locale]/profile/page.tsx
+++ app/[locale]/profile/page.tsx
@@ -1,9 +1,24 @@
-import Personal from './personal/page';
+import { getServerAuthSession } from '~/server/auth';
+
+import FacultySection from '../faculty-and-staff/[employee_id]/[faculty_section]/page';
+import { Personal } from './personal/utils';
 
 export default async function Profile({
-  params,
+  params: { locale },
 }: {
   params: { locale: string };
 }) {
-  return <Personal params={params} />;
+  const session = (await getServerAuthSession())!;
+  if (session.person.type == 'faculty')
+    return (
+      <FacultySection
+        asProfile={{
+          faculty_section: 'qualifications',
+          id: session.person.id,
+          locale,
+        }}
+      />
+    );
+
+  return <Personal locale={locale} id={session.person.id} />;
 }
