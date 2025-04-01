c645493d8808ab2a83e1bf5ed22b5f429852ccc0
heydoyouknowme0 <68548165+heydoyouknowme0@users.noreply.github.com>
2025-04-01 17:43:45 +0000

    fix: faculty profile page

---
+++ app/[locale]/profile/[faculty_section]/page.tsx
@@ -0,0 +1,18 @@
+import FacultySection from '~/app/faculty-and-staff/[employee_id]/[faculty_section]/page';
+import { Translations } from '~/i18n/translations';
+import { getServerAuthSession } from '~/server/auth';
+
+export default async function FacultyProfileSection({
+  params: { locale, faculty_section },
+}: {
+  params: {
+    locale: string;
+    faculty_section: keyof Translations['FacultyAndStaff']['tabs'];
+  };
+}) {
+  const {
+    person: { id },
+  } = (await getServerAuthSession())!;
+
+  return <FacultySection asProfile={{ locale, faculty_section, id }} />;
+}
