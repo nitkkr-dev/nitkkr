fbe2bfa4397fe1b17266a435e482ad6210772161
heydoyouknowme0 <68548165+heydoyouknowme0@users.noreply.github.com>
2025-04-10 12:56:55 +0000

    feat: flexible faculty details

--- server/db/schema/faculty.schema.ts
+++ server/db/schema/faculty.schema.ts
@@ -1,4 +1,4 @@
-import { relations, sql } from 'drizzle-orm';
+import { relations } from 'drizzle-orm';
 import { pgTable, uniqueIndex } from 'drizzle-orm/pg-core';
 
 import {
@@ -39,94 +39,142 @@ export const faculty = pgTable(
 
     // Socials
     googleScholarId: t.text(),
-    orcidId: t.text(),
+    linkedInId: t.text(),
     researchGateId: t.text(),
     scopusId: t.text(),
-
-    // Miscellaneous
-    qualifications: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    areasOfInterest: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    teachingInterests: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    researchInterests: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    patents: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    copyrights: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    publications: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    journals: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    conferences: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    books: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    workshops: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    expertLectures: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    awards: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    outreach: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    eContent: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    researchProjects: t
-      .text()
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
   }),
   (table) => [uniqueIndex('faculty_employee_id_idx').on(table.employeeId)]
 );
+
+// Qualifications Table
+export const qualifications = pgTable('qualifications', (t) => ({
+  id: t.integer().primaryKey(),
+  facultyId: t
+    .integer()
+    .references(() => faculty.id)
+    .notNull(),
+  title: t.text().notNull(),
+  about: t.text().notNull(),
+  location: t.text().notNull(),
+  startDate: t.date().notNull(),
+  endDate: t.date(),
+}));
+
+// Experience Table
+export const experience = pgTable('experience', (t) => ({
+  id: t.serial().primaryKey(),
+  facultyId: t
+    .integer()
+    .references(() => faculty.id)
+    .notNull(),
+  title: t.text().notNull(),
+  description: t.text().notNull(),
+  place: t.text().notNull(),
+  startDate: t.date().notNull(),
+  endDate: t.date().notNull(),
+}));
+
+// Projects Table
+export const projects = pgTable('projects', (t) => ({
+  id: t.serial().primaryKey(),
+  facultyId: t
+    .integer()
+    .references(() => faculty.id)
+    .notNull(),
+  title: t.text().notNull(),
+  field: t.text().notNull(),
+  role: t.text().notNull(),
+  startDate: t.date().notNull(),
+  endDate: t.date().notNull(),
+  tag: t
+    .varchar({
+      enum: ['sponsored', 'unsponsored'],
+    })
+    .notNull(),
+}));
+
+// Continuing Education Table
+export const continuingEducation = pgTable('continuing_education', (t) => ({
+  id: t.serial().primaryKey(),
+  facultyId: t
+    .integer()
+    .references(() => faculty.id)
+    .notNull(),
+  title: t.text().notNull(),
+  field: t.text().notNull(),
+  role: t.text().notNull(),
+  startDate: t.date().notNull(),
+  endDate: t.date().notNull(),
+}));
+
+// Publications Table
+export const publications = pgTable('publications', (t) => ({
+  id: t.integer().primaryKey(),
+  facultyId: t
+    .integer()
+    .references(() => faculty.id)
+    .notNull(),
+  title: t.text().notNull(),
+  field: t.text().notNull(),
+  people: t.text().notNull(),
+  date: t.date().notNull(),
+  tag: t
+    .varchar({
+      enum: ['book', 'journal', 'conference'],
+    })
+    .notNull(),
+}));
+
+// // Research Scholars Table
+// export const researchScholars = pgTable('research_scholars', (t) => ({
+//   id: t.integer().primaryKey(),
+//   facultyId: t
+//     .integer()
+//     .references(() => faculty.id)
+//     .notNull(),
+//   title: t.text().notNull(),
+//   role: t.text().notNull(),
+//   person: t.text().notNull(),
+//   date: t.date().notNull(),
+//   tag: t.text(),
+// }));
+
+// Awards and Honors Table
+export const awardsAndHonors = pgTable('awards_and_honors', (t) => ({
+  id: t.integer().primaryKey(),
+  facultyId: t
+    .integer()
+    .references(() => faculty.id)
+    .notNull(),
+  title: t.text().notNull(),
+  field: t.text().notNull(),
+  date: t.date().notNull(),
+  place: t.text().notNull(),
+}));
+
+// Custom Topics Table
+export const customTopics = pgTable('custom_topics', (t) => ({
+  id: t.serial().primaryKey(),
+  facultyId: t
+    .integer()
+    .references(() => faculty.id)
+    .notNull(),
+  name: t.text().notNull(),
+}));
+
+// Custom Information Table
+export const customInformation = pgTable('custom_information', (t) => ({
+  id: t.serial().primaryKey(),
+  topicId: t
+    .integer()
+    .references(() => customTopics.id, { onDelete: 'cascade' })
+    .notNull(),
+  title: t.text().notNull(),
+  description: t.text(),
+  caption: t.text(),
+  startDate: t.date(),
+  endDate: t.date(),
+}));
 
 export const facultyRelations = relations(faculty, ({ many, one }) => ({
   courseLogs: many(courseLogs),
@@ -142,4 +190,16 @@ export const facultyRelations = relations(faculty, ({ many, one }) => ({
     fields: [faculty.id],
     references: [persons.id],
   }),
+  qualifications: many(qualifications),
+  experience: many(experience),
+  projects: many(projects),
+  continuingEducation: many(continuingEducation),
+  publications: many(publications),
+  //  researchScholars: many(researchScholars),
+  awardsAndHonors: many(awardsAndHonors),
+  customTopics: many(customTopics),
+}));
+
+export const customTopicsRelations = relations(customTopics, ({ many }) => ({
+  customInformation: many(customInformation),
 }));
