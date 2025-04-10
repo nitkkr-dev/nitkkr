f8f949bd83bcd5865b4a2ed2c83d12bf2029c951
heydoyouknowme0 <68548165+heydoyouknowme0@users.noreply.github.com>
2025-04-10 12:56:55 +0000

    feat: flexible faculty details

--- server/db/schema/faculty.schema.ts
+++ server/db/schema/faculty.schema.ts
@@ -1,7 +1,9 @@
-import { relations, sql } from 'drizzle-orm';
+import { relations } from 'drizzle-orm';
 import {
+  date,
   integer,
   pgTable,
+  serial,
   smallint,
   text,
   uniqueIndex,
@@ -26,8 +28,6 @@ export const faculty = pgTable(
       .references(() => persons.id),
     employeeId: varchar('employee_id', { length: 8 }).notNull(),
     officeAddress: varchar('college_address', { length: 16 }).notNull(),
-
-    // Meta
     designation: varchar('designation', {
       enum: [
         'Assistant Professor Grade-I',
@@ -39,78 +39,10 @@ export const faculty = pgTable(
     departmentId: smallint('department_id')
       .references(() => departments.id)
       .notNull(),
-
-    // Socials
     googleScholarId: text('google_scholar_id'),
-    orcidId: text('orcid_id'),
+    linkedInId: text('linked_in_id'),
     researchGateId: text('research_gate_id'),
     scopusId: text('scopus_id'),
-
-    // Miscellaneous
-    qualifications: text('qualifications')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    areasOfInterest: text('areas_of_interest')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    teachingInterests: text('teaching_interests')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    researchInterests: text('research_interests')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    patents: text('patents')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    copyrights: text('copyrights')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    publications: text('publications')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    journals: text('journals')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    conferences: text('conferences')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    books: text('books')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    workshops: text('workshops')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    expertLectures: text('expert_lectures')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    awards: text('awards')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    outreach: text('outreach')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    eContent: text('e_content')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
-    researchProjects: text('research_projects')
-      .array()
-      .default(sql`'{}'`)
-      .notNull(),
   },
   (faculty) => {
     return {
@@ -120,6 +52,123 @@ export const faculty = pgTable(
     };
   }
 );
+
+// Qualifications Table
+export const qualifications = pgTable('qualifications', {
+  id: integer('id').primaryKey(),
+  facultyId: integer('faculty_id')
+    .references(() => faculty.id)
+    .notNull(),
+  title: text('title').notNull(),
+  about: text('about').notNull(),
+  location: text('location').notNull(),
+  startDate: date('start_date').notNull(),
+  endDate: date('end_date'),
+});
+
+// Experience Table
+export const experience = pgTable('experience', {
+  id: serial('id').primaryKey(),
+  facultyId: integer('faculty_id')
+    .references(() => faculty.id)
+    .notNull(),
+  title: text('title').notNull(),
+  description: text('description').notNull(),
+  place: text('place').notNull(),
+  startDate: date('start_date').notNull(),
+  endDate: date('end_date').notNull(),
+});
+
+// Projects Table
+export const projects = pgTable('projects', {
+  id: serial('id').primaryKey(),
+  facultyId: integer('faculty_id')
+    .references(() => faculty.id)
+    .notNull(),
+  title: text('title').notNull(),
+  field: text('field').notNull(),
+  role: text('role').notNull(),
+  startDate: date('start_date').notNull(),
+  endDate: date('end_date').notNull(),
+  tag: varchar('tag', {
+    enum: ['sponsored', 'unsponsored'],
+  }).notNull(),
+});
+
+// Continuing Education Table
+export const continuingEducation = pgTable('continuing_education', {
+  id: serial('id').primaryKey(),
+  facultyId: integer('faculty_id')
+    .references(() => faculty.id)
+    .notNull(),
+  title: text('title').notNull(),
+  field: text('field').notNull(),
+  role: text('role').notNull(),
+  startDate: date('start_date').notNull(),
+  endDate: date('end_date').notNull(),
+});
+
+// Publications Table
+export const publications = pgTable('publications', {
+  id: integer('id').primaryKey(),
+  facultyId: integer('faculty_id')
+    .references(() => faculty.id)
+    .notNull(),
+  title: text('title').notNull(),
+  field: text('field').notNull(),
+  people: text('people').notNull(),
+  date: date('date').notNull(),
+  tag: varchar('tag', {
+    enum: ['book', 'journal', 'conference'],
+  }).notNull(),
+});
+
+// // Research Scholars Table
+// export const researchScholars = pgTable('research_scholars', {
+//   id: integer('id').primaryKey(),
+//   facultyId: integer('faculty_id')
+//     .references(() => faculty.id)
+//     .notNull(),
+//   title: text('title').notNull(),
+//   role: text('role').notNull(),
+//   person: text('person').notNull(),
+//   date: date('date').notNull(),
+//   tag: text('tag'),
+// });
+
+// Awards and Honors Table
+export const awardsAndHonors = pgTable('awards_and_honors', {
+  id: integer('id').primaryKey(),
+  facultyId: integer('faculty_id')
+    .references(() => faculty.id)
+    .notNull(),
+  title: text('title').notNull(),
+  field: text('field').notNull(),
+  date: date('date').notNull(),
+  place: text('place').notNull(),
+});
+
+// Custom Topics Table
+export const customTopics = pgTable('custom_topics', {
+  id: serial('id').primaryKey(),
+  facultyId: integer('faculty_id')
+    .references(() => faculty.id)
+    .notNull(),
+  name: text('name').notNull(),
+});
+
+// Custom Information Table
+export const customInformation = pgTable('custom_information', {
+  id: serial('id').primaryKey(),
+  topicId: integer('topic_id')
+    .references(() => customTopics.id, { onDelete: 'cascade' })
+    .notNull(),
+  title: text('title').notNull(),
+  description: text('description'),
+  caption: text('caption'),
+  startDate: date('start_date'),
+  endDate: date('end_date'),
+});
 
 export const facultyRelations = relations(faculty, ({ many, one }) => ({
   courseLogs: many(courseLogs),
@@ -135,4 +184,16 @@ export const facultyRelations = relations(faculty, ({ many, one }) => ({
     fields: [faculty.id],
     references: [persons.id],
   }),
+  qualifications: many(qualifications),
+  experience: many(experience),
+  projects: many(projects),
+  continuingEducation: many(continuingEducation),
+  publications: many(publications),
+  //  researchScholars: many(researchScholars),
+  awardsAndHonors: many(awardsAndHonors),
+  customTopics: many(customTopics),
+}));
+
+export const customTopicsRelations = relations(customTopics, ({ many }) => ({
+  customInformation: many(customInformation),
 }));
