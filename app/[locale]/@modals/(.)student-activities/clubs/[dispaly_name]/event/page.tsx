6941363931793520f7b97a9091fd4aa4bc7033af
Aryawart-kathpal <aryawart.kathpal2909@gmail.com>
2025-04-10 23:33:22 +0530

    feat:Changed the modal layout in club page

--- app/[locale]/@modals/(.)student-activities/clubs/[dispaly_name]/event/page.tsx
+++ app/[locale]/@modals/(.)student-activities/clubs/[dispaly_name]/event/page.tsx
@@ -70,9 +70,11 @@ export default function EventPage({
   const imageCount = clubEvent.image.length;
 
   return (
-    <Dialog className="container mx-auto flex flex-col items-start space-y-6 rounded-lg border border-primary-500 bg-background p-6 shadow-lg md:flex-row md:items-center md:space-x-6 md:space-y-0">
+    <Dialog className="mx-auto flex max-w-5xl flex-col items-center rounded-xl border border-primary-500 bg-background p-8 shadow-xl mx-4">
+      <h2 className="text-primary-800 mb-4 text-3xl font-bold self-start">{clubEvent.title}</h2>
+
       <GalleryCarousel
-        className="hidden h-80 w-full max-w-sm items-center justify-center overflow-hidden rounded-lg md:flex md:max-w-md"
+        className="mb-6 w-full overflow-hidden rounded-lg"
         itemClassName=""
       >
         {[...Array<number>(imageCount)].map((_, index) => (
@@ -81,43 +83,25 @@ export default function EventPage({
             height={400}
             key={index}
             src={clubEvent.image[index]}
-            width={400}
-            className="h-full w-full rounded-lg object-cover"
-          />
+            width={1000}
+            className="w-full rounded-lg object-cover h-40 sm:h-60 md:h-80 lg:h-[400px]"
+        />
+        
         ))}
       </GalleryCarousel>
-      <section className="flex-1">
-        <h2 className="text-primary-800 mb-4 text-2xl font-bold">
-          {clubEvent.title}
-        </h2>
-        <GalleryCarousel
-          className="flex h-80 w-full max-w-sm items-center justify-center overflow-hidden rounded-lg md:hidden md:max-w-md"
-          itemClassName=""
-        >
-          {[...Array<number>(imageCount)].map((_, index) => (
-            <Image
-              alt={String(index)}
-              height={400}
-              key={index}
-              src={clubEvent.image[index]}
-              width={400}
-              className="h-full w-full rounded-lg object-cover"
-            />
-          ))}
-        </GalleryCarousel>
-        <br className="md:hidden" />
-        <p className="text-gray-700 text-base leading-relaxed">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore quis
-          optio adipisci, voluptates nobis eos nisi tempora eius assumenda
-          maxime et velit reiciendis reprehenderit libero aut rem, vero, dolorum
-          animi! Voluptatum assumenda rerum, non consequatur labore vitae
-          repudiandae maxime beatae in doloribus itaque, quaerat dolorem
-          nesciunt modi quas provident officia, necessitatibus ut amet qui
-          voluptatibus facere. Aut aliquid veritatis cum, nihil, minima
-          laudantium perferendis assumenda doloribus quam aspernatur nulla
-          porro!
-        </p>
-      </section>
+
+      <p className="text-gray-700 text-base leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore quis
+        optio adipisci, voluptates nobis eos nisi tempora eius assumenda
+        maxime et velit reiciendis reprehenderit libero aut rem, vero, dolorum
+        animi! Voluptatum assumenda rerum, non consequatur labore vitae
+        repudiandae maxime beatae in doloribus itaque, quaerat dolorem
+        nesciunt modi quas provident officia, necessitatibus ut amet qui
+        voluptatibus facere. Aut aliquid veritatis cum, nihil, minima
+        laudantium perferendis assumenda doloribus quam aspernatur nulla
+        porro!
+      </p>
     </Dialog>
+
   );
 }
