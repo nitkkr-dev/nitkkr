76f6b2a0bf832805c8a95dbf753abbbd3a4663a9
Abhay Agarwal <161469723+Abhay145@users.noreply.github.com>
2025-04-11 15:20:19 +0530

    Error Page (#249)

--- i18n/translations.ts
+++ i18n/translations.ts
@@ -337,6 +337,7 @@ export interface Translations {
     NoResult: { title: string; description: string };
     Unauthorised: { title: string; description: string };
     WorkInProgress: { title: string; description: string };
+    NotAcceptable: { title: string; description: string };
   };
   StudentActivities: {
     title: string;
