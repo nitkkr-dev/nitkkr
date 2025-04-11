564761f6079ec9850943609d010b41b0f6f05a44
som-04 <somyasati14@gmail.com>
2025-04-11 09:59:17 +0000

    fix:UI/Translation

--- app/[locale]/[...catchAll]/page.tsx
+++ app/[locale]/[...catchAll]/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { getTranslations } from '~/i18n/translations';
+import { cn } from '~/lib/utils';
 
 export default async function NotFound({
   params: { locale },
@@ -11,47 +12,64 @@ export default async function NotFound({
   const text = (await getTranslations(locale)).NotFound;
 
   return (
-    <article className="flex h-screen items-center justify-center px-4 text-error md:px-12 lg:px-24">
-      <div className="flex w-full max-w-6xl flex-col items-center space-y-8 text-center md:flex-row md:space-x-20 md:space-y-0">
-        <div className="w-full md:w-1/2">
+    <article className="container flex h-screen items-center justify-center text-error">
+      <div
+        className={cn(
+          'flex w-full max-w-6xl flex-col items-center',
+          'space-y-8 md:flex-row md:space-x-20 md:space-y-0'
+        )}
+      >
+        <section className="w-full text-center md:w-1/2">
           <h1
-            className="text-center font-fingerpaint text-[80px] leading-none sm:text-[120px] md:text-center md:text-[150px] lg:text-[190px]"
-            style={{
-              background: 'linear-gradient(to top, #E7695F, #E13F32)',
-              WebkitBackgroundClip: 'text',
-              color: 'transparent',
-            }}
+            className={cn(
+              'bg-clip-text font-fingerpaint text-transparent',
+              'bg-gradient-to-t from-primary-100 to-primary-500',
+              'text-[80px] sm:text-[120px] md:text-[150px] lg:text-[190px]',
+              'leading-none'
+            )}
           >
             {text.title}
           </h1>
+
           <p
-            className="font-poppins text-center text-[24px] leading-[32px] sm:text-[32px] sm:leading-[40px] md:text-center md:text-[40px] md:leading-[48px]"
-            style={{
-              fontWeight: 500,
-              maxWidth: '361px',
-              margin: '0 auto',
-            }}
+            className={cn(
+              'font-Poppins font-bold',
+              'text-[24px] sm:text-[32px] md:text-[40px]',
+              'leading-[32px] sm:leading-[40px] md:leading-[48px]',
+              'text-primary-300'
+            )}
           >
             {text.description}
           </p>
-          <div className="mt-4 w-full text-center">
+
+          <div className="mt-4">
             <Link
               href={`/${locale}`}
-              className="decoration-skip-ink-none block text-center text-[30px] leading-[38px] decoration-[from-font] sm:text-[35px] sm:leading-[44px] md:text-[40px] md:leading-[48px]"
+              className={cn(
+                'block text-center font-Poppins font-bold',
+                'text-[24px] sm:text-[32px] md:text-[40px]',
+                'leading-[38px] sm:leading-[44px] md:leading-[48px]',
+                'decoration-skip-ink-none decoration-[from-font]',
+                'text-primary-300'
+              )}
             >
               {text.backHome}
             </Link>
           </div>
-        </div>
-        <div className="flex w-full justify-center md:w-1/2">
+        </section>
+
+        <section className="flex w-full justify-center md:w-1/2">
           <Image
             src="assets/error-2.png"
             alt="Work in Progress"
             width={661}
             height={513}
-            className="h-auto w-[300px] opacity-50 sm:w-[400px] md:w-[500px] lg:w-[661px]"
+            className={cn(
+              'h-auto opacity-50',
+              'w-[300px] sm:w-[400px] md:w-[500px] lg:w-[661px]'
+            )}
           />
-        </div>
+        </section>
       </div>
     </article>
   );
