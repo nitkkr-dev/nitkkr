c15b5e6acd2a557bc7cb683e87f472cbe9b45959
heydoyouknowme0 <68548165+heydoyouknowme0@users.noreply.github.com>
2025-04-12 11:31:37 +0000

    feat: faculty page frontend

---
+++ app/[locale]/faculty-and-staff/[employee_id]/layout.tsx
@@ -0,0 +1,28 @@
+import { union } from 'drizzle-orm/pg-core';
+import { type ReactNode } from 'react';
+
+import { db, faculty, staff } from '~/server/db';
+
+import { FacultyOrStaffComponent } from '../utils';
+
+export async function generateStaticParams() {
+  const facultyIds = db
+    .select({ employee_id: faculty.employeeId })
+    .from(faculty);
+  const staffIds = db.select({ employee_id: staff.employeeId }).from(staff);
+  return await union(facultyIds, staffIds);
+}
+
+export default async function FacultyOrStaffLayout({
+  children,
+  params: { locale, employee_id },
+}: {
+  children?: ReactNode;
+  params: { locale: string; employee_id: string };
+}) {
+  return (
+    <FacultyOrStaffComponent locale={locale} employeeId={employee_id}>
+      {children}
+    </FacultyOrStaffComponent>
+  );
+}
