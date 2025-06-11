a2249a1e98799cd44a2ab697e7dd4576aa2250cb
aryansri-19 <75990590+aryansri-19@users.noreply.github.com>
2025-06-11 13:27:42 +0530

    refactor: removed redundant events schema file

--- server/db/schema/event.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, smallserial, text } from 'drizzle-orm/pg-core';
-
-export const event = pgTable('event', {
-  eventId: smallserial('event_id').primaryKey(),
-  title: text('title').notNull(),
-  description: text('description').notNull(),
-  date: text('date').notNull(),
-  venue: text('venue'),
-});
