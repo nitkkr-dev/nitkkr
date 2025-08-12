e74a1af3766fb6b559aa397fc96d6d38ee282ab5
Yashika Choudhary <161009245+yashika1221@users.noreply.github.com>
2025-08-13 04:00:42 +0530

    Added Copyrights and Designs (#309)
    Co-authored-by: Soumil Jain <soumiljain221@gmail.com>
    Co-authored-by: Aryawart-kathpal <aryawart.kathpal2909@gmail.com>

--- i18n/hi.ts
+++ i18n/hi.ts
@@ -701,6 +701,29 @@ const text: Translations = {
     about: 'परिचय',
     objectives: 'लक्ष्य',
   },
+  CopyrightsAndDesigns: {
+    title: 'प्रतिलिपि अधिकार एवं डिज़ाइन',
+    description: [
+      'एनआईटी कुरुक्षेत्र के संकाय सदस्यों / कर्मचारियों / छात्रों द्वारा प्राप्त कॉपीराइट्स नीचे सूचीबद्ध हैं:',
+      'एनआईटी कुरुक्षेत्र के संकाय सदस्यों / कर्मचारियों / छात्रों के नाम दर्ज डिज़ाइनों की सूची नीचे दी गई है:',
+    ],
+    headers: {
+      copyrights: {
+        serialNo: 'क्रम संख्या',
+        grantYear: 'अनुदान वर्ष',
+        regNo: 'पंजीकरण संख्या',
+        title: 'शीर्षक',
+        author: 'लेखक',
+      },
+      designs: {
+        serialNo: 'क्रम संख्या',
+        yearOfAcceptance: 'स्वीकृति वर्ष',
+        applicationNo: 'आवेदन संख्या',
+        title: 'शीर्षक',
+        creator: 'निर्माता',
+      },
+    },
+  },
   Search: {
     placeholder: 'त्वरित खोज...',
     categories: {
