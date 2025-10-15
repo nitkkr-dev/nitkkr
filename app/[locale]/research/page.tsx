c2bf6210ca7696ddf6d917b94610d6086f1a9752
Aryawart Kathpal <132134276+Aryawart-kathpal@users.noreply.github.com>
2025-10-15 14:45:50 +0530

    Feat/populate/research and consultancy (#396)
    Populated research and consultancy

--- app/[locale]/research/page.tsx
+++ app/[locale]/research/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { sql } from 'drizzle-orm';
 
 import Heading from '~/components/heading';
 import Loading from '~/components/loading';
@@ -17,10 +18,16 @@ import { PaginationWithLogic } from '~/components/pagination/pagination';
 import { getTranslations } from '~/i18n/translations';
 import { getS3Url } from '~/server/s3';
 import { db } from '~/server/db';
-import type { copyrights, designs, patents } from '~/server/db/schema';
+import type {
+  copyrights,
+  designs,
+  patents,
+  researchAndConsultancy,
+} from '~/server/db/schema';
 type PatentsTable = typeof patents.$inferSelect;
 type CopyrightsTable = typeof copyrights.$inferSelect;
 type DesignsTable = typeof designs.$inferSelect;
+type ResearchAndConsultancyTable = typeof researchAndConsultancy.$inferSelect;
 
 export default async function PatentsAndTechnology({
   params: { locale },
@@ -67,44 +74,20 @@ export default async function PatentsAndTechnology({
   const patents = await db.query.patents.findMany();
   const copyrights = await db.query.copyrights.findMany();
   const designs = await db.query.designs.findMany();
+  const researchAndConsultancy = await db.query.researchAndConsultancy.findMany(
+    {
+      with: {
+        faculty: {
+          with: {
+            person: true,
+            department: true,
+          },
+        },
+      },
+      orderBy: (rc) => sql`SUBSTRING(${rc.year}, 1, 4)::integer DESC`, // sorting by year to latest
+    }
+  );
 
-  const staticResearch = [
-    {
-      facultyName: 'Dr. S.K. Patidar',
-      department: 'Civil Engineering',
-      facultyCode: 'skp',
-      totalJobs: '23',
-      totalAmount: '501500',
-    },
-    {
-      facultyName: 'Dr. S.K. Patidar',
-      department: 'Civil Engineering',
-      facultyCode: 'skp',
-      totalJobs: '23',
-      totalAmount: '501500',
-    },
-    {
-      facultyName: 'Dr. S.K. Patidar',
-      department: 'Civil Engineering',
-      facultyCode: 'skp',
-      totalJobs: '23',
-      totalAmount: '501500',
-    },
-    {
-      facultyName: 'Dr. S.K. Patidar',
-      department: 'Civil Engineering',
-      facultyCode: 'skp',
-      totalJobs: '23',
-      totalAmount: '501500',
-    },
-    {
-      facultyName: 'Dr. S.K. Patidar',
-      department: 'Civil Engineering',
-      facultyCode: 'skp',
-      totalJobs: '23',
-      totalAmount: '501500',
-    },
-  ];
   const staticMemorandum = [
     {
       organization: 'CSIR-Central Road Research Institute, New Delhi',
@@ -212,7 +195,7 @@ export default async function PatentsAndTechnology({
   const base = getS3Url();
   // Get the total count for pagination
   const getResearchCount = async () => {
-    const count = staticResearch.length; // Replace with your actual DB call
+    const count = researchAndConsultancy.length; // Replace with your actual DB call
     return [{ count }];
   };
   const getProjectCount = async () => {
@@ -278,9 +261,9 @@ export default async function PatentsAndTechnology({
                   text.research.number,
                   text.research.faculty,
                   text.research.department,
-                  text.research.facultyCode,
                   text.research.totalJobs,
                   text.research.total,
+                  text.research.year,
                 ].map((headerText, index) => (
                   <TableHead key={index}>{headerText}</TableHead>
                 ))}
@@ -297,7 +280,7 @@ export default async function PatentsAndTechnology({
                 }
               >
                 <ResearchTable
-                  tableData={staticResearch}
+                  tableData={researchAndConsultancy}
                   currentPage={currentPage}
                   itemsPerPage={10}
                 />
@@ -724,13 +707,12 @@ const ResearchTable = ({
   currentPage,
   itemsPerPage = 10,
 }: {
-  tableData: {
-    facultyName: string;
-    department: string;
-    facultyCode: string;
-    totalJobs: string;
-    totalAmount: string;
-  }[];
+  tableData: (ResearchAndConsultancyTable & {
+    faculty: {
+      person: { name: string };
+      department: { name: string };
+    };
+  })[];
   currentPage: number;
   itemsPerPage?: number;
 }) => {
@@ -742,11 +724,11 @@ const ResearchTable = ({
       {visibleData.map((item, index) => {
         const cellData = [
           startIndex + index + 1,
-          item.facultyName,
-          item.department,
-          item.facultyCode,
-          item.totalJobs,
+          item.faculty?.person?.name || 'N/A',
+          item.faculty?.department?.name || 'N/A',
+          item.totalNoOfJobs,
           item.totalAmount,
+          item.year,
         ];
 
         return (
