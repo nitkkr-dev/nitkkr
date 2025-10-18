4e73ac7dd4ae0b6c12b271cb77d6ae9ed7727d60
Aryawart-kathpal <aryawart.kathpal2909@gmail.com>
2025-10-18 23:41:28 +0530

    fix: Added mapping to schema

--- server/db/schema/clubs.schema.ts
+++ server/db/schema/clubs.schema.ts
@@ -1,38 +1,38 @@
 import { relations } from 'drizzle-orm';
 import { pgTable } from 'drizzle-orm/pg-core';
 
-import {
-  clubMembers,
-  clubSocials,
-  departments,
-  events,
-  notifications,
-  persons,
-} from '.';
+import { clubMembers } from './club-members.schema';
+import { clubSocials } from './club-socials.schema';
+import { departments } from './departments.schema';
+import { events } from './events.schema';
+import { notifications } from './notifications.schema';
+import { persons } from './persons.schema';
 import { clubFacultyHeads } from './club-faculty-heads.schema';
 
 export const clubs = pgTable('clubs', (t) => ({
-  id: t.smallserial().primaryKey(),
-  name: t.varchar({ length: 128 }).notNull(),
-  urlName: t.varchar({ length: 128 }).notNull(),
-  alias: t.varchar({ length: 16 }),
-  tagline: t.varchar({ length: 256 }).notNull(),
-  email: t.varchar({ length: 256 }).notNull(),
-  aboutUs: t.varchar().notNull(),
-  howToJoinUs: t.varchar().notNull(),
-  whyToJoinUs: t.varchar().notNull(),
+  id: t.smallserial('id').primaryKey(),
+  name: t.varchar('name', { length: 128 }).notNull(),
+  urlName: t.varchar('url_name', { length: 128 }).notNull(),
+  alias: t.varchar('alias', { length: 16 }),
+  tagline: t.varchar('tagline', { length: 256 }).notNull(),
+  email: t.varchar('email', { length: 256 }).notNull(),
+  aboutUs: t.varchar('about_us').notNull(),
+  howToJoinUs: t.varchar('how_to_join_us').notNull(),
+  whyToJoinUs: t.varchar('why_to_join_us').notNull(),
   category: t
-    .varchar({ enum: ['committee', 'cultural', 'crew', 'technical'] })
+    .varchar('category', {
+      enum: ['committee', 'cultural', 'crew', 'technical'],
+    })
     .notNull(),
-  departmentId: t.smallint().references(() => departments.id), // not necessary that club/society is related with a department
-  isActive: t.boolean().default(true).notNull(),
-  createdOn: t.date({ mode: 'date' }).defaultNow().notNull(),
+  departmentId: t.smallint('department_id').references(() => departments.id),
+  isActive: t.boolean('is_active').default(true).notNull(),
+  createdOn: t.date('created_on', { mode: 'date' }).defaultNow().notNull(),
   updatedAt: t
-    .timestamp()
+    .timestamp('updated_at')
     .$onUpdate(() => new Date())
     .notNull(),
   updatedBy: t
-    .integer()
+    .integer('updated_by')
     .references(() => persons.id)
     .notNull(),
 }));
