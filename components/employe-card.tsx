294b56dd7e027d08b7ba94921d061dba3f2d1a9f
kartik-vats <ks25152005@gmail.com>
2025-11-19 18:41:13 +0530

    fix: Removed linting errors

--- components/employe-card.tsx
+++ components/employe-card.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { FaPhone } from 'react-icons/fa6';
 import { MdEmail } from 'react-icons/md';
+
 import { cn } from '~/lib/utils';
 
 export default function EmployeeCard({
@@ -21,7 +22,7 @@ export default function EmployeeCard({
   return (
     <article
       className={cn(
-        'flex items-center gap-3 sm:gap-4 rounded-lg border border-gray-300 p-3 sm:p-5 shadow-sm mx-auto transition-all duration-300',
+        'border-gray-300 mx-auto flex items-center gap-3 rounded-lg border p-3 shadow-sm transition-all duration-300 sm:gap-4 sm:p-5',
         className
       )}
       style={{
@@ -34,7 +35,7 @@ export default function EmployeeCard({
       <div className="flex-shrink-0 overflow-hidden rounded-md shadow-sm">
         <Image
           alt={name}
-          className="h-[70px] w-[70px] sm:h-[90px] sm:w-[90px] md:h-[110px] md:w-[110px] rounded-md object-cover"
+          className="h-[70px] w-[70px] rounded-md object-cover sm:h-[90px] sm:w-[90px] md:h-[110px] md:w-[110px]"
           height={110}
           width={110}
           src={image}
@@ -43,17 +44,19 @@ export default function EmployeeCard({
 
       {/* Info */}
       <div className="flex flex-col justify-center space-y-1 text-[13px] sm:text-sm md:text-base">
-        <h3 className="text-base sm:text-lg font-semibold text-red-700 md:text-xl">{name}</h3>
+        <h3 className="text-red-700 text-base font-semibold sm:text-lg md:text-xl">
+          {name}
+        </h3>
         <p className="text-gray-800 font-semibold">{position}</p>
 
         <div className="mt-1 space-y-[2px] sm:space-y-1">
-          <p className="flex items-center gap-2 text-gray-700 break-all">
+          <p className="text-gray-700 flex items-center gap-2 break-all">
             <MdEmail className="text-red-700 flex-shrink-0" />
             <a href={`mailto:${email}`} className="hover:underline">
               {email}
             </a>
           </p>
-          <p className="flex items-center gap-2 text-gray-700">
+          <p className="text-gray-700 flex items-center gap-2">
             <FaPhone className="text-red-700 flex-shrink-0" />
             {phone}
           </p>
