a59682acfeaecece560c4a9ffb78e476edd0311f
yashika.1221 <yashika122010@gmail.com>
2026-01-17 21:31:24 +0530

    senate, building-and-work

---
+++ server/db/schema/building-and-work-composition.schema.ts
@@ -0,0 +1,20 @@
+import { pgTable } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+
+export const buildingAndWorkComposition = pgTable(
+  'building_and_work_composition',
+  (t) => ({
+    id: t.serial('id').primaryKey(),
+    name: t
+      .text()
+      .array()
+      .notNull()
+      .default(sql`'{}'::text[]`),
+    servedAs: t.varchar('served_as', { length: 256 }).notNull(),
+    createdAt: t.timestamp('created_at').defaultNow().notNull(),
+    updatedAt: t
+      .timestamp('updated_at')
+      .$onUpdate(() => new Date())
+      .notNull(),
+  })
+);
