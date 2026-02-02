60a93c5e4a24534d63f70837b010fee6955d5ed1
Saksham1143me <150987031+Saksham1143me@users.noreply.github.com>
2026-02-02 18:12:05 +0530

    fix - clubs page modal with aligned  image and text (#491)

--- app/[locale]/student-activities/clubs/[display_name]/event-section.tsx
+++ app/[locale]/student-activities/clubs/[display_name]/event-section.tsx
@@ -56,7 +56,7 @@ export default function EventsSection({
         onOpenChange={(open) => !open && setSelectedEvent(null)}
       >
         <DialogContent
-          className="z-[100] mx-4 mx-auto flex max-w-[90vw] flex-col items-center rounded-xl border border-primary-500 
+          className="z-[100] w-7/12 mx-auto flex max-w-[90vw] flex-col items-center rounded-xl border border-primary-500 
 bg-background p-8 shadow-xl md:max-w-[80vw]
 lg:max-w-[1100px]"
         >
