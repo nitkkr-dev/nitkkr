0c931be688e38a3262f85a864e323ecfb869d175
SurAyush <ayushsur26@gmail.com>
2026-02-23 00:48:06 +0530

    placement stats tables created and data pushed from different repo

---
+++ server/db/schema/placement-stats-ug.schema.ts
@@ -0,0 +1,57 @@
+import { sql } from 'drizzle-orm';
+import { pgTable } from 'drizzle-orm/pg-core';
+
+export const ugPlacementStats = pgTable('placement_stats-UG', (t) => ({
+    id: t
+        .smallserial()
+        .primaryKey(),
+    academic_session: t
+        .text()
+        .notNull(),
+    programme: t
+        .text()
+        .notNull(),
+    number_of_eligible: t
+        .smallint()
+        .notNull(),
+    number_of_placed: t
+        .smallint()
+        .notNull(),
+    median_package: t
+        .numeric({
+            precision: 8, 
+            scale: 3
+        })
+        .notNull(),
+    average_package: t
+        .numeric({
+            precision: 8, 
+            scale: 3
+        })
+        .notNull(),
+    lowest_package: t
+        .numeric({
+            precision: 8, 
+            scale: 3
+        })
+        .notNull(),
+    highest_package: t
+        .numeric({
+            precision: 8, 
+            scale: 3
+        })
+        .notNull(),
+    perecentage_placed: t
+        .numeric({
+            precision: 7, 
+            scale: 4
+        })
+        .generatedAlwaysAs(
+            sql`
+                CASE
+                    WHEN number_of_eligible = 0 THEN 0
+                    ELSE (number_of_placed::numeric / number_of_eligible) * 100
+                END
+            `
+        ),
+}));
