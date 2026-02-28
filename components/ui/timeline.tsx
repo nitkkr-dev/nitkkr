b4006cdfbc4a01c7bd5191f254586e445d33a914
Abhay145 <161469723+Abhay145@users.noreply.github.com>
2026-02-28 12:25:56 +0530

    Replaced motion with framer

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -4,7 +4,7 @@ import {
   useMotionValueEvent,
   useScroll,
   useTransform,
-} from 'motion/react';
+} from 'framer-motion';
 import React, { useEffect, useRef, useState } from 'react';
 
 interface TimelineEntry {
